1d78fbc58800e1b311818f36347a5a23e7cfeaa5
benlipkin <bglipkin@gmail.com>
2021-10-07 09:11:06 -0400

    hi

--- langbrainscore/interface/encoder.py
+++ langbrainscore/interface/encoder.py
@@ -1,11 +1,51 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 import numpy as np
 
-class Encoder(ABC):
 
+class Encoder(ABC):
     def __init__(self) -> None:
         pass
 
+    @abstractmethod()
     def encode(self, X: np.array) -> np.array:
-        NotImplemented
+        raise NotImplementedError()
+
+
+class BrainEncoder(Encoder):
+    def __init__(self) -> None:
+        super().__init__(self)
+        pass
+
+
+class CompEncoder(Encoder):
+    def __init__(self) -> None:
+        super().__init__(self)
+        pass
+
+
+class Pereira18Encoder(BrainEncoder):
+    def __init__(self, bmid) -> None:
+        super().__init__(self)
+        self._bmid = bmid
+
+    def encode(self, X: np.array) -> np.array:
+        pass
+
+
+class HFEncoder(CompEncoder):
+    def __init__(self, hfid) -> None:
+        super().__init__(self)
+        self._hfid = hfid
+
+    def encode(self, X: np.array) -> np.array:
+        pass
+
+
+class PTEncoder(CompEncoder):
+    def __init__(self, ptid) -> None:
+        super().__init__(self)
+        self._ptid = hfid
+
+    def encode(self, X: np.array) -> np.array:
+        pass
