ee374d1e62c1343c2abd72fcc8d40ce52c72e706
aalok-sathe <asathe@mit.edu>
2021-10-21 16:48:37 -0400

    implement some basic dataset methods

--- langbrainscore/dataset/dataset.py
+++ langbrainscore/dataset/dataset.py
@@ -1,11 +1,21 @@
 import typing
+import numpy as np
+import pandas as pd
 
 class Dataset:
-    _stimuli_path_or_uri = None
+    _stimuli_path = None
     _stimuli = None
+    _stim_metadata = None
 
-    def __init__(self) -> None:
-        ...
+    def __init__(self, stimuli: typing.Union[list, np.array, pd.DataFrame], 
+                #  stimuli_path: str = None,
+                 stim_metadata: typing.Union[pd.DataFrame] = None) -> None:
+
+        print('setting stimuli in "DataSet"!')
+        self._stimuli = stimuli
+
+        if stim_metadata:
+            self._stim_metadata = stim_metadata
 
     @property
     def stimuli(self):
@@ -23,14 +33,48 @@ class BrainDataset(Dataset):
     In general, data must be supplied in the mapping
             stimulus --> neuroid
         where `neuroid` is the basic unit of brain recording (it may be a voxel, an electrode, etc).
-    '''
-    _data_path_or_uri = None
-    _recorded_data = None
 
-    def __init__(self, data_path_or_uri) -> None:
-        _data_path_or_uri = data_path_or_uri
+    Inputs:
+        recorded_data: <n_stimuli> x <n_neuroids>
+        metadata: <n_stimuli> x -1
+    '''
+    _recorded_data = None
+    _recording_metadata = None
+
+    def __init__(self,
+                 stimuli: typing.Union[list, pd.DataFrame, np.array],
+                 recorded_data: typing.Union[np.ndarray, pd.DataFrame, str],
+                 stimuli_metadata: typing.Union[pd.DataFrame, str] = None,
+                 recording_metadata: typing.Union[pd.DataFrame, str] = None) -> None:
+                 
+        # TODOs for the (distant) future:
+        # scipy.io.loadmat stuf
+        # h5py 2 GB limit issue
+        
+        super().__init__(stimuli, stimuli_metadata)
+        self._recorded_data = recorded_data
+    
+        if not recording_metadata is None: 
+          # ^-- may not be necessary; users should always provide subject_ids
+          # for cross-validation 
+            self._recording_metadata = recording_metadata
+        else:
+            raise ValueError(f'forgot to pass recording metadata?')
+
+        # make sure the data dimensions make sense
+        num_stimuli = len(self.stimuli)
+        num_neuroids = self._recorded_data.shape[1]
+
+        # recorded_data contains a row per stimulus
+        assert num_stimuli == self._recorded_data.shape[0]
+        # recording_metadata contains a row per neuroid
+        assert num_neuroids == self._recording_metadata.shape[0]
+
 
     @property
     def recorded_data(self):
-        ...
+        return self._recorded_data
 
+    @property
+    def recording_metadata(self):
+        return self._recording_metadata
---
+++ langbrainscore/encoder/__init__.py
@@ -0,0 +1,2 @@
+
+from langbrainscore.interface.encoder import *
