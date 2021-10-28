90a6d49e7440a7f97b9ad54a593142d8edda9eea
aalok-sathe <asathe@mit.edu>
2021-10-28 17:59:11 -0400

    more progress on the mock dataset script

--- mock_data.py
+++ mock_data.py
@@ -40,4 +40,23 @@ mock_brain_encoder = lbs.interface.encoder.BrainEncoder()
 brain_encoded_data = mock_brain_encoder.encode(fake_neuro_dataset)
 print(brain_encoded_data, brain_encoded_data.shape)
 
-pdb.set_trace()
+# pdb.set_trace()
+
+print('fitting a mapping')
+
+ridge_mapping = lbs.mapping.Mapping('ridge')
+
+output = ridge_mapping.map_cv(brain_encoded_data + np.random.rand(*brain_encoded_data.shape), 
+                              brain_encoded_data[:,:400])
+
+print(output[0], output[0][0].shape, output[0][1].shape)
+
+metric = lbs.metrics.pearson_r.pearson_r
+
+all_Y_pred = output[0][0]
+all_Y_test = output[0][1]
+
+# print(all_Y_pred)
+
+pearson_rs = [metric(all_Y_pred[:, i], all_Y_test[:, i]) for i in range(all_Y_pred.shape[1])]
+print(pearson_rs, len(pearson_rs))
