ae15881813aa1985e3120987e076770d574ac15f
aalok-sathe <asathe@mit.edu>
2021-10-28 15:41:11 -0400

    import stmts in the init

--- langbrainscore/__init__.py
+++ langbrainscore/__init__.py
@@ -1,6 +1,9 @@
 
-import langbrainscore.encoding
+import langbrainscore.dataset
+import langbrainscore.encoder
 import langbrainscore.interface
 import langbrainscore.mapping
 import langbrainscore.metrics
 import langbrainscore.utils
+
+from langbrainscore.interface.encoder import * 
