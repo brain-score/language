3d5895ab077469f695c0464db6809845374093f4
Greta Tuckute <gretatu@mit.edu>
2021-12-02 16:27:29 -0500

    first draft of get_activations function

--- langbrainscore/interface/encoder.py
+++ langbrainscore/interface/encoder.py
@@ -6,6 +6,85 @@ import pandas as pd
 
 import langbrainscore
 
+## Functions (probably to migrate later on)
+
+def flatten_activations(activations):
+    """
+    Convert activations into dataframe format
+    Input: dict, key = layer, item = 2D array of stimuli x units
+    Output: pd dataframe, stimulus ID x MultiIndex (layer, unit)
+    """
+    labels = []
+    arr_flat = []
+    for layer, arr in activations.items():
+        arr = np.array(arr) # for each layer
+        arr_flat.append(arr)
+        for i in range(arr.shape[1]): # across units
+            labels.append((layer, i))
+    arr_flat = np.concatenate(arr_flat, axis=1) # concatenated activations across layers
+    df = pd.DataFrame(arr_flat)
+    df.columns = pd.MultiIndex.from_tuples(labels) # rows: stimuli, columns: units
+    return df
+
+def get_activations(model, tokenizer, stimuli, emb_method='last-tok',
+                    case='lower', punc='strip-all', punc_exceptions=[],
+                    norm=None, outlier_removal=None,
+                    verbose=True):
+    """
+    Obtain activations from (HF) models.
+    
+    :param model: model object, HF
+    :paral tokenizer: tokenizer object, HF
+    :param stimuli: list/array, [to do, edit] containing strings
+    :param emb_method: str, denoting how to obtain sentence embedding
+    :param case: str, denoting which casing to use
+    :param punc: str, denoting which operations to perform regarding punctuation
+    :param punc_exceptions: list, denoting which punctuation to NOT strip if punc == 'strip-all'
+    :param norm: str, denoting how to normalize the embeddings 
+    :param outlier_removel: str, denoting whether to remove 'outliers' from the embedding
+        
+    Returns dict with key = layer, item = 2D array of stimuli x units
+    """
+    
+    model.eval() 
+    n_layer = model.config.n_layer
+    max_n_tokens = model.config.n_positions
+    states_sentences = defaultdict(list)
+    
+    if verbose:
+        print(f'Computing activations for {len(sents)} sentences')
+
+    for count, sent in enumerate(stimuli):
+        if case == 'lower': # todo deal with these nicely
+            sent = sent.lower()
+        if case == 'upper':
+            sent = sent.upper()
+            
+        input_ids = torch.tensor(tokenizer.encode(sent))
+
+        result_model = model(input_ids, output_hidden_states=True, return_dict=True)
+        hidden_states = result_model['hidden_states'] # 3D tensor of dims: [batch, tokens, emb size]
+        
+        for i in range(n_layer+1): # for each layer
+            if emb_method == 'last-tok':
+                state = hidden_states[i].squeeze()[-1, :].detach().numpy() # get last token
+            elif emb_method == 'mean-tok':
+                state = torch.mean(hidden_states[i].squeeze(), dim=0).detach().numpy() # mean over tokens
+            elif emb_method == 'median-tok':
+                state = torch.median(hidden_states[i].squeeze(), dim=0).detach().numpy() # median over tokens
+            elif emb_method == 'sum-tok':
+                state = torch.sum(hidden_states[i].squeeze(), dim=0).detach().numpy() # sum over tokens
+            elif emb_method == 'all-tok':
+                raise NotImplementedError()
+            else:
+                print('Sentence embedding method not implemented')
+                raise NotImplementedError()
+
+            states_sentences[i].append(state)
+            
+    return states_sentences
+
+
 class Encoder(ABC):
     def __init__(self) -> None:
         pass
@@ -71,8 +150,7 @@ class ANNEncoder(Encoder):
         """[summary]
         
         # Todo: Arguments: 
-        Embedding method (emb_method): last-tok, mean-tok, median-tok, all-tok, 
-        
+        Embedding method (emb_method): last-tok, mean-tok, median-tok, sum-tok, all-tok, 
         Casing (case): lower, upper, None (no edits)
         Punctuation (punc): strip-all, None
         Punctuation exceptions, i.e. what NOT to strip (punc_exceptions): default: []
