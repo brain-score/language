91947fce519b6a57defbf9b3752f2d642412fd1c
aalok-sathe <asathe@mit.edu>
2021-12-03 11:20:21 -0500

    move brain encoder implementation from interface to encoder submodule

--- langbrainscore/encoder/brain.py
+++ langbrainscore/encoder/brain.py
@@ -1,11 +1,47 @@
-from langbrainscore.interface.encoder import BrainEncoder
+from langbrainscore.interface.encoder import _BrainEncoder
 import numpy as np
 
-# class Pereira18Encoder(BrainEncoder):
-#     def __init__(self, bmid) -> None:
-#         super().__init__(self)
-#         self._bmid = bmid
 
-#     def encode(self, X: np.array) -> np.array:
-#         pass
+class BrainEncoder(_BrainEncoder):
+    '''
+    This class provides a wrapper around real-world brain data of various kinds
+        which may be: 
+            - neuroimaging [fMRI, PET]
+            - physiological [ERP, MEG, ECOG]
+            - behavioral [RT, Eye-tracking]
+    across several subjects. The class implements `BrainEncoder.encode` which takes in
+    a collection of stimuli (typically `np.array` or `list`) 
+    '''
 
+    _dataset: 'langbrainscore.dataset.Dataset' = None
+
+    def __init__(self, dataset = None) -> None:
+        # if not isinstance(dataset, langbrainscore.dataset.BrainDataset):
+        #     raise TypeError(f"dataset must be of type `langbrainscore.dataset.BrainDataset`, not {type(dataset)}")
+        self._dataset = dataset
+
+    @property
+    def dataset(self) -> 'langbrainscore.dataset.Dataset':
+        return self._dataset
+
+    # @typing.overload
+    # def encode(self, stimuli: typing.Union[np.array, list]): ...
+    def encode(self, dataset: 'langbrainscore.dataset.BrainDataSet' = None):
+        """returns an "encoding" of stimuli (passed in as a BrainDataset)
+
+        Args:
+            stimuli (langbrainscore.dataset.BrainDataset):
+
+        Returns:
+            pd.DataFrame: neural recordings for each stimulus, multi-indexed 
+                          by layer (trivially just 1 layer)
+        """        
+        
+        dataset = dataset or self.dataset
+
+        if (timeid_dims := dataset._dataset.dims['timeid']) > 1:
+            return dataset._dataset.mean('timeid')
+        elif timeid_dims == 1:
+            return dataset._dataset.squeeze('timeid')
+        else:
+            raise ValueError(f'timeid has invalid shape {timeid_dims}')
