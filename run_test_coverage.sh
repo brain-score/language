b2aefacda0bfeda19a5c99a0c9a2dfb3249c4be4
aalok-sathe <aalok.sathe@richmond.edu>
2022-02-06 12:08:57 -0500

    bugfix in call to coverage.py

--- run_test_coverage.sh
+++ run_test_coverage.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -x
 
-mkdir -p html/ test-results/
+rm -r html test-results htmlcov
+mkdir -p html htmlcov test-results
 
 coverage run -m pytest --junitxml=test-results/pytest.xml --html=test-results/pytest.html --self-contained-html || true # junit.xml
-coverage html -m langbrainscore/**/*.py
+coverage html langbrainscore/**/*.py
 mypy -m langbrainscore --config-file pyproject.toml --junit-xml test-results/mypy.xml --html-report test-results/typecheck || true
 
 mv test-results html/
