eb10504fd1ecb430dc1e17aff1e9ef587d1b15cf
aalok-sathe <aalok.sathe@richmond.edu>
2022-02-06 14:16:21 -0500

    the issue was coverage.py outputting a stray .gitignore file in the coverage output dir >:(

--- run_test_coverage.sh
+++ run_test_coverage.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
-set -x
+set -xe
 
 # cleanup to start fresh
-rm -rf html test-results coverage
+rm -rf html test-results codecov
 mkdir -p html test-results
 
 # run pytest using coverage
 coverage run -m pytest --junitxml=test-results/tests.xml --html=test-results/tests.html --self-contained-html || true # junit.xml
 
 # generate coverage report
-coverage html -d coverage langbrainscore/**/*.py 
-coverage xml -o test-results/coverage.xml langbrainscore/**/*.py 
+coverage html -d codecov langbrainscore/**/*.py && rm -f codecov/.gitignore
+coverage xml -o test-results/codecov.xml langbrainscore/**/*.py 
 
 # run static type checking using mypy
 mypy -m langbrainscore --config-file pyproject.toml --junit-xml test-results/typing.xml --html-report test-results/typing || true
 
 # move all the reports and tests into the deployment folder
 mv test-results html/
-mv coverage html/test-results/
+mv codecov html/test-results/
 
 # sanity check in CI
 ls -lah html html/test-results
