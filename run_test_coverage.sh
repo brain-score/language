df2fc326797b989b94eb8e0b55432887c15c4df2
aalok-sathe <aalok.sathe@richmond.edu>
2022-02-06 12:54:23 -0500

    why is coverage report still not appering

--- run_test_coverage.sh
+++ run_test_coverage.sh
@@ -4,11 +4,11 @@ set -x
 rm -r html test-results htmlcov
 mkdir -p html htmlcov test-results
 
-coverage run -m pytest --junitxml=test-results/pytest.xml --html=test-results/pytest.html --self-contained-html || true # junit.xml
+coverage run -m pytest --junitxml=test-results/pytest.xml --html=test-results/index.html --self-contained-html || true # junit.xml
 coverage html langbrainscore/**/*.py
-mypy -m langbrainscore --config-file pyproject.toml --junit-xml test-results/mypy.xml --html-report test-results/typecheck || true
+mv htmlcov html/
 
+mypy -m langbrainscore --config-file pyproject.toml --junit-xml test-results/mypy.xml --html-report test-results/typecheck || true
 mv test-results html/
-mv htmlcov html/test-results/
 
 ls -lah html html/test-results
