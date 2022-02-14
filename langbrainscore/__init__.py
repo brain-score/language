61bfa1b8e364321f1515d7481ae106103550d153
aalok-sathe <aalok.sathe@richmond.edu>
2022-02-14 14:28:34 -0500

    root of the module namespace import stmts

--- langbrainscore/__init__.py
+++ langbrainscore/__init__.py
@@ -2,8 +2,10 @@
 import langbrainscore.dataset
 import langbrainscore.encoder
 import langbrainscore.interface
-import langbrainscore.mapping_tools
+import langbrainscore.mapping
 import langbrainscore.metrics
 import langbrainscore.utils
+import langbrainscore.brainscore
 
-from langbrainscore.encoder import * 
+from langbrainscore.encoder import PTEncoder, HuggingFaceEncoder 
+from langbrainscore.brainscore import BrainScore
