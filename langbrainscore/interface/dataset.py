5b3e37421733ddfa9ea258485fd5f710c062bc19
aalok-sathe <asathe@mit.edu>
2022-04-14 10:18:04 -0400

    add a "dataset_name" attribute on _Dataset for identifiability. defaults to None

--- langbrainscore/interface/dataset.py
+++ langbrainscore/interface/dataset.py
@@ -6,9 +6,9 @@ class _Dataset(_Cacheable, ABC):
     """
     wrapper class for xarray DataArray that confirms format adheres to interface.
     """
+    dataset_name: str = None
 
-
-    def __init__(self, xr_obj: xr.DataArray) -> "_Dataset":
+    def __init__(self, xr_obj: xr.DataArray, dataset_name: str = None, _skip_checks: bool = False) -> "_Dataset":
         """
         accepts an xarray with the following core
         dimensions: sampleid, neuroid, timeid
@@ -18,10 +18,19 @@ class _Dataset(_Cacheable, ABC):
         Args:
             xr_obj (xr.DataArray): xarray object with core dimensions and coordinates
         """
-        dims = ("sampleid", "neuroid", "timeid")
-        coords = dims + ("stimulus", "subject")
-        assert isinstance(xr_obj, xr.DataArray)
-        assert xr_obj.ndim == len(dims)
-        assert all([dim in xr_obj.dims for dim in dims])
-        assert all([coord in xr_obj.coords for coord in coords])
-        self._xr_obj = xr_obj
+
+        if xr_obj is not None:
+            try:
+                self.dataset_name = xr_obj.attrs['name']
+            except KeyError:
+                pass
+        self.dataset_name = self.dataset_name or dataset_name
+
+        if not _skip_checks:
+            dims = ("sampleid", "neuroid", "timeid")
+            coords = dims + ("stimulus", "subject")
+            assert isinstance(xr_obj, xr.DataArray)
+            assert xr_obj.ndim == len(dims)
+            assert all([dim in xr_obj.dims for dim in dims])
+            assert all([coord in xr_obj.coords for coord in coords])
+            self._xr_obj = xr_obj
