154f1435d0af9fc97063590a224413b3ded98839
aalok-sathe <asathe@mit.edu>
2022-04-14 10:17:18 -0400

    MWE implementation of caching and recovering from cache for _Cacheable-implementing classes

--- langbrainscore/interface/cacheable.py
+++ langbrainscore/interface/cacheable.py
@@ -3,24 +3,49 @@
 import pickle
 import typing
 from abc import ABC, abstractclassmethod, abstractmethod
+from numbers import Number
 from pathlib import Path
-import yaml
+
 import xarray as xr
-from langbrainscore.utils.cache import get_cache_root
+import yaml
+from langbrainscore.utils.cache import get_cache_directory, pathify
 from langbrainscore.utils.logging import log
 
 T = typing.TypeVar('T')
 
-class _Cacheable(ABC):
+@typing.runtime_checkable
+class _Cacheable(typing.Protocol):
     '''
     A class used to define a common interface for Object caching in LangBrainscore
     '''
+
+    def __eq__(self: T, __o: typing.Union[T, '_Cacheable']) -> bool:
+        
+        def checkattr(key) -> bool:
+            try:
+                if getattr(self, key) != getattr(__o, key):
+                    return False
+            except AttributeError:
+                return False
+
+        for key, ob in vars(self).items():
+            if isinstance(ob, (str, Number, bool, _Cacheable, type(None), xr.DataArray)):
+                if not checkattr(key): 
+                    return False
+        else:
+            return True
+
 
     # @abstractclassmethod
     # @classmethod
     def _get_xarray_objects(self) -> typing.Iterable[str]:
         '''
-        returns all (visible) attributes of self that are instances of xarray
+        returns the *names* of all attributes of self that are instances of xarray
+        NOTE: this method should be implemented by any subclass irrespective of instance
+            state so that in the future we can support loading from cache without having
+            to re-run the pipeline (and thereby assign attributes as appropriate) 
+        by default, just goes over all the objects and returns their names if they are instances
+        of `xr.DataArray`
         '''
         keys = []
         for key, ob in vars(self).items():
@@ -32,39 +57,152 @@ class _Cacheable(ABC):
     # def _get_meta_attributes(cls) -> typing.Iterable[str]:
     #     return ()
 
-    def to_cache(self, identifier_string: str, overwrite = True,
-                 xarray_serialization_backend='to_zarr', cache_dir = None) -> None:
+
+    def __repr__(self) -> str:
+        '''
+        default, broad implementation to support our use case.
+        constructs a string by concatenating all str, numeric, boolean 
+        attributes of self, as well as all the representations of Cacheable 
+        instances that are attributes of self.
+        '''
+
+        sep = '#'
+        rep = f'<{self.__class__.__name__}'
+        for key, ob in vars(self).items():
+            if isinstance(ob, (str, Number, bool, _Cacheable, type(None))):
+                if isinstance(ob, _Cacheable):
+                    rep += f'{sep}{key}={ob.identifier_string}'
+                else:
+                    rep += f'{sep}{key}={ob}'
+        return rep + '>'
+
+
+    # @abstractmethod
+    @property
+    def identifier_string(self):
+        ''' 
+        This property aims to return an unambiguous representation of this _Cacheable
+        instance, complete with all scalar parameters used to initialize it, and any
+        _Cacheable instances that are attributes of this object.
+
+        Unless overridden, makes a call to `repr`
+        '''
+        return repr(self)
+        return ' '.join(repr(self).split())
+
+
+    def to_cache(self, identifier_string: str = None, overwrite = True,
+                 cache_dir = None, xarray_serialization_backend='to_zarr') -> Path:
         '''
         dump this object to cache. this method implementation will serve
         as the default implementation. it is recommended that this be left
         as-is for compatibility with caching across the library.
-        '''
-        root = Path(cache_dir or get_cache_root()).expanduser().resolve()
-        root /= identifier_string
-        root /= self.__class__.__name__
-        root.mkdir(exist_ok=True, parents=True)
-        log(f'caching {self} to {root}')
 
-        with (root / 'xarray_object_names.yml').open('w') as f:
+        Args:
+            identifier_string (str): a unique identifier string to identify this cache
+                instance by (optional; by default, the .identifier_string property is used)
+            overwrite (bool): whether to overwrite existing cache by the same identity,
+                if it exists. if False, an exce
+        '''
+        if cache_dir:
+            cache = get_cache_directory(cache_dir, calling_class=self.__class__.__name__)
+        else:
+            cache = get_cache_directory(calling_class=self.__class__.__name__)
+        
+        root, subdir = cache.root, cache.subdir
+        # now we use "subdir" to be our working directory to dump this cache object
+        subdir /= (identifier_string or self.identifier_string)
+        subdir.mkdir(parents=True, exist_ok=overwrite)
+        log(f'caching {self} to {subdir}')
+
+        with (subdir / 'xarray_object_names.yml').open('w') as f:
             yaml.dump(self._get_xarray_objects(), f, yaml.SafeDumper)
+        with (subdir / 'id.txt').open('w') as f:
+            f.write(self.identifier_string)
 
         kwargs = {}
         if overwrite and 'zarr' in xarray_serialization_backend: 
             kwargs.update({'mode':'w'})
         for ob_name in self._get_xarray_objects():
             ob = getattr(self, ob_name)
-            tgt_dir = root / (ob_name + '.xr')
+            tgt_dir = subdir / (ob_name + '.xr')
+            dump_object_fn = getattr(ob.to_dataset(name='data'), xarray_serialization_backend)
+            dump_object_fn(tgt_dir, **kwargs)
+       
+        cacheable_ptrs = {}
+        meta_attributes = {}
+        for key, ob in vars(self).items():
+            if isinstance(ob, _Cacheable):
+                dest = ob.to_cache(identifier_string=identifier_string, overwrite=overwrite,
+                                   xarray_serialization_backend=xarray_serialization_backend,
+                                   cache_dir=cache_dir)
+                cacheable_ptrs[key] = dest
+            elif isinstance(ob, (str, Number, bool, _Cacheable, type(None))):
+                meta_attributes[key] = ob
+        with (subdir / 'meta_attributes.yml').open('w') as f:
+            yaml.dump(meta_attributes, f, yaml.SafeDumper)
+        with (subdir / 'cacheable_object_pointers.yml').open('w') as f:
+            yaml.dump(cacheable_ptrs, f, yaml.SafeDumper)
+
+        return subdir
+
+
+    def load_cache(self, identifier_string: str = None, overwrite: bool = True,
+                   xarray_deserialization_backend='open_zarr', cache_dir = None) -> Path:
+        '''load attribute objects from cache onto the existing initialized object (self)'''
+        
+        if cache_dir:
+            cache = get_cache_directory(cache_dir, calling_class=self.__class__.__name__)
+        else:
+            cache = get_cache_directory(calling_class=self.__class__.__name__)
+        
+        root, subdir = cache.root, cache.subdir
+        # now we use "subdir" as our working directory to dump this cache object
+        subdir /= (identifier_string or self.identifier_string)
+        log(f'loading attributes of {self} from {subdir}')
+
+        with (subdir / 'xarray_object_names.yml').open('r') as f:
+            self_xarray_objects = yaml.load(f, yaml.SafeLoader)
+        
+        with (subdir / 'id.txt').open('r') as f:
+            if (identifier_string or self.identifier_string) != (cached_identifier_str := f.read()):
+                if not overwrite:
+                    raise ValueError(f'mismatch in identifier string of self ({self.identifier_string}) and '
+                                     f'cached object ({cached_identifier_str}); overwriting is disabled.')
+                else:
+                    log(f'mismatch in identifier string of self ({self.identifier_string}) and '
+                        f'cached object ({cached_identifier_str}); overwriting anyway.')
+
+        kwargs = {}
+        for ob_name in self_xarray_objects:
+            tgt_dir = subdir / (ob_name + '.xr')
+            load_object_fn = getattr(xr, xarray_deserialization_backend)
+            ob = load_object_fn(tgt_dir, **kwargs)
+            setattr(self, ob_name, ob.data)
+       
+        with (subdir / 'cacheable_object_pointers.yml').open('r') as f:
+            cacheable_ptrs: dict = yaml.load(f, yaml.SafeLoader)
+
+        # calls `load_cache` on all attributes that are also `_Cacheable` instances
+        # and thus implement the `load_cache` method
+        for key, ptr in cacheable_ptrs.items():
+            try:
+                ob = getattr(self, key)
+                ob.load_cache(identifier_string=identifier_string, overwrite=overwrite,
+                              xarray_deserialization_backend=xarray_deserialization_backend,
+                              cache_dir=cache_dir)
+            except AttributeError:
+                log(f'`load_cache` currently only supports loading xarray objects or initialized `_Cacheable` objects')
+        
+        with (subdir / 'meta_attributes.yml').open('r') as f:
+            meta_attributes: dict = yaml.load(f, yaml.SafeLoader)
+        for key, ob in meta_attributes.items():
+            setattr(self, key, ob)
             
-            dump_object = getattr(ob.to_dataset(name='data'), xarray_serialization_backend)
-            dump_object(tgt_dir, **kwargs)
-
-        with (root / f'{self.__class__.__name__}.pkl').open('wb') as f:
-            pickle.dump(self, f)
-
 
     # NB comment from Guido: https://github.com/python/typing/issues/58#issuecomment-194569410
     @classmethod
-    def from_cache(cls, identifier_string: str, 
+    def from_cache(cls: typing.Callable[..., T], identifier_string: str, 
                    xarray_deserialization_backend='open_zarr',
                    cache_dir = None) -> T:
         '''
@@ -77,22 +215,11 @@ class _Cacheable(ABC):
             return ob
             
         '''
-        root = Path(cache_dir or get_cache_root()).expanduser().resolve()
-        root /= identifier_string
-        root /= cls.__name__
-        root.mkdir(exist_ok=True, parents=True)
-        log(f'loading cache for {cls} from {root} {identifier_string}')
 
-        with (root/f'{cls.__name__}.pkl').open('rb') as f:
-            ob = pickle.load(f)
-
-        with (root / 'xarray_object_names.yml').open('r') as f:
-            xarray_object_names = yaml.load(f, yaml.SafeLoader)
-
-        for attr in xarray_object_names:
-            tgt_dir = root / (attr + '.xr')
-            load_object = getattr(xr, xarray_deserialization_backend)
-            xarray_instance = load_object(tgt_dir)
-            setattr(ob, attr, xarray_instance.data)
-
-        return ob
+        Duck = type(cls.__name__, (cls,), {'__init__': (lambda _: None)})
+        duck = Duck()
+        duck.load_cache(identifier_string, 
+                        overwrite=True,
+                        xarray_deserialization_backend=xarray_deserialization_backend, 
+                        cache_dir=cache_dir)
+        return duck
--- langbrainscore/utils/cache.py
+++ langbrainscore/utils/cache.py
@@ -6,20 +6,52 @@ managing a disk-backed LRU cache, etc.
 import typing
 from pathlib import Path
 import os
+from dataclasses import dataclass
 
-def get_cache_root(prefix: typing.Union[str, Path] = '~/.cache') -> Path:
+@dataclass
+class CacheDescriptor:
     '''
+    A class to conveniently hold various paths within the LBS_CACHE directory structure
+    '''
+    root: Path
+    subdir: Path
+    # human_readable_name: Path
+    
+    def mkdirs(self):
+        '''creates directories if they don't already exist'''
+        self.subdir.mkdir(parents=True, exist_ok=True)
 
+
+def pathify(fpth: typing.Union[Path, str, typing.Any]) -> Path:
     '''
-    if 'LBS_CACHE' in os.environ:
+    returns a resolved `Path` object after expanding user and shorthands/symlinks
+    '''
+    return Path(fpth).expanduser().resolve()
+
+
+def get_cache_directory(prefix: typing.Union[str, Path] = '~/.cache',
+                        calling_class = None,
+                        # subdirs: typing.List[str] = ['dataset', 'encoder', 'mapping', 'metric', 'brainscore'],
+                        # randomize: bool = False
+                        ) -> CacheDescriptor:
+    '''
+    returns the "root" of langbrainscore cache. any instance-specific runs must make sure
+    to make their own directory structure within this root and identify themselves uniquely
+    so as not to get overwritten by other runs
+    '''
+    if 'LBS_CACHE' in os.environ: # if environment variable is specified, use that with first priority
         prefix = os.environ['LBS_CACHE']
 
-    prefix = Path(prefix).expanduser().resolve()
+    prefix = pathify(prefix)
     root = prefix / 'langbrainscore'
 
-    # act = root / 'encoder_activations'
-    # data = root / 'datasets' 
-    # results = root / 'results'
+    # if randomize:
+    #     import randomname
+    #     while (root / (human_readable := randomname.generate())).exists():
+    #         pass
 
-    root.mkdir(parents=True, exist_ok=True)
-    return root
+    CD = CacheDescriptor(root=root, **{'subdir': root / subdir for subdir in [calling_class or 'uncategorized']})
+    CD.mkdirs()
+    return CD
+
+
