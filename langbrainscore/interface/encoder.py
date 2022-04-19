961cef62126c55b21508dc5928beebcf669861dd
aalok-sathe <asathe@mit.edu>
2022-04-19 18:57:12 -0400

    write an `EncoderRepresentations` dataclass to support caching of ANN outputs

--- langbrainscore/interface/encoder.py
+++ langbrainscore/interface/encoder.py
@@ -1,12 +1,17 @@
+'''
+Interface and (partial) base implementation classes for Encoders and EncodedRepresentations
+'''
+
+import typing
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
 import xarray as xr
 from langbrainscore.dataset import Dataset
 from langbrainscore.interface.cacheable import _Cacheable
 
 
-# TODO: class _Encoder(_Cacheable, ABC):
-class _Encoder(ABC):
+class _Encoder(_Cacheable, ABC):
     """
     Interface for *Encoder classes.
     Must implement an `encode` method that operates on a Dataset object.
@@ -28,14 +33,21 @@ class _Encoder(ABC):
 
 
 class _ModelEncoder(_Encoder):
-    def __init__(self, model_id: str) -> "_ModelEncoder":
+    def __init__(self, model_id: str, **kwargs) -> "_ModelEncoder":
+        """This class is intended to be an interface for all ANN subclasses,
+            including HuggingFaceEncoder, and, in the future, other kinds of
+            ANN encoders
+
+        Args:
+            model_id (str): _description_
+
+        Returns:
+            _ModelEncoder: _description_
+        """
+
         self._model_id = model_id
-
-
-    def __repr__(self) -> str:
-        return f"<{self.__class__} {self._model_id}>"
-    def __str__(self) -> str:
-        return repr(self)
+        for k, v in kwargs.items():
+            setattr(self, k, v)
 
 
     @abstractmethod
@@ -45,9 +57,24 @@ class _ModelEncoder(_Encoder):
 
         Args:
             langbrainscore.dataset.Dataset: a Dataset object with a member `xarray.DataArray` 
-                instance containing stimuli
+                instance (`Dataset._xr_obj`) containing stimuli
 
         Returns:
             xr.DataArray: Model representations of each stimulus in brain dataset
         """
         raise NotImplementedError
+
+
+@dataclass(repr=False, eq=False, frozen=True, kw_only=True)
+class EncoderRepresentations(_Cacheable):
+    '''
+    a class to hold the encoded representations output from an `_Encoder.encode` method
+    '''
+    dataset: Dataset # pointer to the dataset these are the EncodedRepresentations of
+    representations: xr.DataArray # the xarray holding representations
+
+    context_dimension: str = None
+    bidirectional: bool = False
+    emb_case: typing.Union[str, None] = "lower"
+    emb_aggregation: typing.Union[str, None, typing.Callable] = "last"
+    emb_preproc: typing.Tuple[str] = ()
