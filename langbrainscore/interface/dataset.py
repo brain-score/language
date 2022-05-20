e2d523a0cae97eecfb307c910fb9b8c7f21192f1
aalok-sathe <asathe@mit.edu>
2022-05-20 16:48:05 -0400

    "fix" dtypes of the xarray as part of Dataset() init

--- langbrainscore/interface/dataset.py
+++ langbrainscore/interface/dataset.py
@@ -1,14 +1,19 @@
 import xarray as xr
 from langbrainscore.interface.cacheable import _Cacheable
+from langbrainscore.utils.xarray import fix_xr_dtypes
 from abc import ABC
+
 
 class _Dataset(_Cacheable, ABC):
     """
     wrapper class for xarray DataArray that confirms format adheres to interface.
     """
+
     dataset_name: str = None
 
-    def __init__(self, xr_obj: xr.DataArray, dataset_name: str = None, _skip_checks: bool = False) -> "_Dataset":
+    def __init__(
+        self, xr_obj: xr.DataArray, dataset_name: str = None, _skip_checks: bool = False
+    ) -> "_Dataset":
         """
         accepts an xarray with the following core
         dimensions: sampleid, neuroid, timeid
@@ -21,7 +26,7 @@ class _Dataset(_Cacheable, ABC):
 
         if xr_obj is not None:
             try:
-                self.dataset_name = xr_obj.attrs['name']
+                self.dataset_name = xr_obj.attrs["name"]
             except KeyError:
                 pass
         self.dataset_name = self.dataset_name or dataset_name
@@ -33,4 +38,4 @@ class _Dataset(_Cacheable, ABC):
             assert xr_obj.ndim == len(dims)
             assert all([dim in xr_obj.dims for dim in dims])
             assert all([coord in xr_obj.coords for coord in coords])
-            self._xr_obj = xr_obj
+            self._xr_obj = fix_xr_dtypes(xr_obj)
