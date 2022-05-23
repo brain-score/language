11697971b2781ee36f2ce11f3ce48afb73289405
aalok-sathe <asathe@mit.edu>
2022-05-23 13:40:50 -0400

    bring back original packaging script; handle caching and loading cached dataset

--- examples/test_mean_froi_pereira2018_firstsessions.py
+++ examples/test_mean_froi_pereira2018_firstsessions.py
@@ -10,25 +10,91 @@ from tqdm import tqdm
 import langbrainscore as lbs
 from langbrainscore.utils.logging import log
 from langbrainscore.utils.xarray import collapse_multidim_coord
-from langbrainscore.benchmarks import pereira2018
 
-# import pereira2018_mean_froi_nat_stories
+# from langbrainscore.benchmarks import pereira2018
 
-os.environ["VERBOSE"] = "1"
+
+def package_mean_froi_pereira2018_firstsess():
+    mpf = pd.read_csv(
+        f"{Path(__file__).parents[1] / 'data/Pereira_FirstSession_TrialEffectSizes_20220223.csv'}"
+    )
+    # mpf = pd.read_csv(f"{'../data/Pereira_FirstSession_TrialEffectSizes_20220223.csv'}")
+    mpf = mpf.sort_values(by=["UID", "Session", "Experiment", "Stim"])
+    subj_xrs = []
+    neuroidx = 0
+    for uid in tqdm(mpf.UID.unique()):
+        mpf_subj = mpf[mpf.UID == uid]
+        sess_xrs = []
+        for sess in mpf_subj.Session.unique():
+            mpf_sess = mpf_subj[mpf_subj.Session == sess]
+            roi_filt = [any(n in c for n in ["Lang", "MD"]) for c in mpf_sess.columns]
+            mpf_rois = mpf_sess.iloc[:, roi_filt]
+            data_array = np.expand_dims(mpf_rois.values, 2)
+            sess_xr = xr.DataArray(
+                data_array,
+                dims=("sampleid", "neuroid", "timeid"),
+                coords={
+                    "sampleid": (
+                        np.arange(0, 384)
+                        if data_array.shape[0] == 384
+                        else np.arange(384, 384 + 243)
+                    ),
+                    "neuroid": np.arange(neuroidx, neuroidx + data_array.shape[1]),
+                    "timeid": np.arange(data_array.shape[2]),
+                    "stimulus": ("sampleid", mpf_sess.Sentence.str.strip('"')),
+                    "passage": (
+                        "sampleid",
+                        list(map(lambda p_s: p_s.split("_")[0], mpf_sess.Stim)),
+                    ),
+                    "experiment": ("sampleid", mpf_sess.Experiment),
+                    "session": (
+                        "neuroid",
+                        np.array(
+                            [mpf_sess.Session.values[0]] * data_array.shape[1],
+                            dtype=object,
+                        ),
+                    ),
+                    "subject": (
+                        "neuroid",
+                        [mpf_sess.UID.values[0]] * data_array.shape[1],
+                    ),
+                    "roi": ("neuroid", mpf_rois.columns),
+                },
+            )
+            sess_xrs.append(sess_xr)
+        neuroidx += data_array.shape[1]
+        subj_xr = xr.concat(sess_xrs, dim="sampleid")
+        subj_xrs.append(subj_xr)
+    mpf_xr = xr.concat(subj_xrs, dim="neuroid")
+    mpf_xr = collapse_multidim_coord(mpf_xr, "stimulus", "sampleid")
+    mpf_xr = collapse_multidim_coord(mpf_xr, "passage", "sampleid")
+    mpf_xr = collapse_multidim_coord(mpf_xr, "experiment", "sampleid")
+    mpf_xr = collapse_multidim_coord(mpf_xr, "session", "neuroid")
+    return mpf_xr
 
 
 def main():
-    mpf_xr = pereira2018.pereira2018_mean_froi_nat_stories()
-    mpf_dataset = lbs.dataset.Dataset(
-        mpf_xr.isel(neuroid=mpf_xr.roi.str.contains("Lang")),
-        dataset_name="Pereira2018LangfROIs",
-    )
+    # mpf_xr = pereira2018.pereira2018_mean_froi_nat_stories()
+    mpf_xr = package_mean_froi_pereira2018_firstsess()
+
+    try:
+        mpf_dataset = lbs.dataset.Dataset(
+            xr.DataArray(),
+            dataset_name="Pereira2018LangfROIs",
+            _skip_checks=True,
+        )
+        mpf_dataset.load_cache()
+    except FileNotFoundError:
+        mpf_dataset = lbs.dataset.Dataset(
+            mpf_xr.isel(neuroid=mpf_xr.roi.str.contains("Lang")),
+            dataset_name="Pereira2018LangfROIs",
+        )
 
     log(f"stimuli: {mpf_dataset.stimuli.values}")
-    mpf_dataset.to_cache("test_mpf_dataset_cache", cache_dir="./cache")
-    mpf_dataset = lbs.dataset.Dataset.from_cache(
-        "test_mpf_dataset_cache", cache_dir="./cache"
-    )
+    # mpf_dataset.to_cache("test_mpf_dataset_cache", cache_dir="./cache")
+    # mpf_dataset = lbs.dataset.Dataset.from_cache(
+    #     "test_mpf_dataset_cache", cache_dir="./cache"
+    # )
     log(f"stimuli: {mpf_dataset.stimuli.values}")
 
     # Initialize brain and ANN encoders
@@ -56,8 +122,8 @@ def main():
 
     # Model card
     ann_modelcard = ann_enc.get_modelcard()
-    ann_enc.get_layer_sparsity(ann_encoded_dataset=ann_enc_mpf)
-    ann_enc.get_explainable_variance(ann_encoded_dataset=ann_enc_mpf)
+    lbs.utils.encoder.get_layer_sparsity(ann_encoded_dataset=ann_enc_mpf)
+    lbs.utils.encoder.get_explainable_variance(ann_encoded_dataset=ann_enc_mpf)
 
     # Initialize mapping and metric
     ann_enc_mpf = ann_enc_mpf.isel(
