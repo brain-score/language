b5ac8f10945bb283f5340e126574c51fc9501683
aalok-sathe <asathe@mit.edu>
2022-05-23 11:25:15 -0400

    use built-in packaging method to declutter test script

--- examples/test_mean_froi_pereira2018_firstsessions.py
+++ examples/test_mean_froi_pereira2018_firstsessions.py
@@ -10,70 +10,15 @@ from tqdm import tqdm
 import langbrainscore as lbs
 from langbrainscore.utils.logging import log
 from langbrainscore.utils.xarray import collapse_multidim_coord
+from langbrainscore.benchmarks import pereira2018
 
-os.environ['VERBOSE'] = '1'
+# import pereira2018_mean_froi_nat_stories
 
-def package_mean_froi_pereira2018_firstsess():
-    mpf = pd.read_csv(
-        f"{Path(__file__).parents[1] / 'data/Pereira_FirstSession_TrialEffectSizes_20220223.csv'}"
-    )
-    # mpf = pd.read_csv(f"{'../data/Pereira_FirstSession_TrialEffectSizes_20220223.csv'}")
-    mpf = mpf.sort_values(by=["UID", "Session", "Experiment", "Stim"])
-    subj_xrs = []
-    neuroidx = 0
-    for uid in tqdm(mpf.UID.unique()):
-        mpf_subj = mpf[mpf.UID == uid]
-        sess_xrs = []
-        for sess in mpf_subj.Session.unique():
-            mpf_sess = mpf_subj[mpf_subj.Session == sess]
-            roi_filt = [any(n in c for n in ["Lang", "MD"]) for c in mpf_sess.columns]
-            mpf_rois = mpf_sess.iloc[:, roi_filt]
-            data_array = np.expand_dims(mpf_rois.values, 2)
-            sess_xr = xr.DataArray(
-                data_array,
-                dims=("sampleid", "neuroid", "timeid"),
-                coords={
-                    "sampleid": (
-                        np.arange(0, 384)
-                        if data_array.shape[0] == 384
-                        else np.arange(384, 384 + 243)
-                    ),
-                    "neuroid": np.arange(neuroidx, neuroidx + data_array.shape[1]),
-                    "timeid": np.arange(data_array.shape[2]),
-                    "stimulus": ("sampleid", mpf_sess.Sentence.str.strip('"')),
-                    "passage": (
-                        "sampleid",
-                        list(map(lambda p_s: p_s.split("_")[0], mpf_sess.Stim)),
-                    ),
-                    "experiment": ("sampleid", mpf_sess.Experiment),
-                    "session": (
-                        "neuroid",
-                        np.array(
-                            [mpf_sess.Session.values[0]] * data_array.shape[1],
-                            dtype=object,
-                        ),
-                    ),
-                    "subject": (
-                        "neuroid",
-                        [mpf_sess.UID.values[0]] * data_array.shape[1],
-                    ),
-                    "roi": ("neuroid", mpf_rois.columns),
-                },
-            )
-            sess_xrs.append(sess_xr)
-        neuroidx += data_array.shape[1]
-        subj_xr = xr.concat(sess_xrs, dim="sampleid")
-        subj_xrs.append(subj_xr)
-    mpf_xr = xr.concat(subj_xrs, dim="neuroid")
-    mpf_xr = collapse_multidim_coord(mpf_xr, "stimulus", "sampleid")
-    mpf_xr = collapse_multidim_coord(mpf_xr, "passage", "sampleid")
-    mpf_xr = collapse_multidim_coord(mpf_xr, "experiment", "sampleid")
-    mpf_xr = collapse_multidim_coord(mpf_xr, "session", "neuroid")
-    return mpf_xr
+os.environ["VERBOSE"] = "1"
 
 
 def main():
-    mpf_xr = package_mean_froi_pereira2018_firstsess()
+    mpf_xr = pereira2018.pereira2018_mean_froi_nat_stories()
     mpf_dataset = lbs.dataset.Dataset(
         mpf_xr.isel(neuroid=mpf_xr.roi.str.contains("Lang")),
         dataset_name="Pereira2018LangfROIs",
@@ -93,6 +38,7 @@ def main():
         emb_preproc=tuple(),
         context_dimension="passage",
         bidirectional=True,
+        emb_aggregation="first",
         # model_id="distilgpt2", emb_preproc=tuple(), context_dimension="passage"
     )
 
