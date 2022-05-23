75d3f59707482c5a01c44c2f58aee44116a2e334
Greta Tuckute <gretatu@mit.edu>
2022-05-23 14:56:41 -0400

    mapping between names and config files in HF

--- langbrainscore/utils/resources.py
+++ langbrainscore/utils/resources.py
@@ -11,3 +11,23 @@ preprocessor_classes = {
     'pca': PCA(n_components=10),
     'gaussian_random_projection': GaussianRandomProjection(n_components=10)
 }
+
+model_classes = ['gpt', 'bert']  # continuously update based on new model classes supported
+config_name_mappings = {
+                        "gpt": {
+                            "n_layers": "n_layer",
+                            "n_attention_heads": "n_head",
+                            "n_context_len": "n_ctx",
+                            "vocab_size": "vocab_size",
+                            "hidden_emb_dim": "n_embd",
+                            "hidden_activation_function": "activation_function"},
+                        
+                        "bert": {
+                            "n_layers": "num_hidden_layers",
+                            "n_attention_heads": "num_attention_heads",
+                            "n_context_len": "max_position_embeddings",
+                            "vocab_size": "vocab_size",
+                            "hidden_emb_dim": "hidden_size",
+                            "hidden_activation_function": "hidden_act"}
+                        }
+                        
