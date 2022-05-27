61b013cfe64d995b8f590e89de2694f39dc70099
aalok-sathe <asathe@mit.edu>
2022-05-27 16:14:34 -0400

    refactor class Metric, which does not inherit from `_Cacheable`, so would be problematic. instead, directly use specific `_Metric` subclasses such as CKA, RSA, KendallTau

--- langbrainscore/metrics/__init__.py
+++ langbrainscore/metrics/__init__.py
@@ -1,1 +1,32 @@
-from langbrainscore.metrics.metric import *
+"""submodule housing metrics used to evaluate similarity of representations"""
+
+import typing
+from langbrainscore.metrics.metric import (
+    CKA,
+    FisherCorr,
+    PearsonR,
+    RSA,
+    ClassificationAccuracy,
+    RMSE,
+    KendallTau,
+    SpearmanRho,
+    # Metric,
+)
+
+metric_classes = {
+    "cka": CKA,
+    "rsa": RSA,
+    "pearsonr": PearsonR,
+    "spearmanrho": SpearmanRho,
+    "fishercorr": FisherCorr,
+    "kendalltau": KendallTau,
+    "rmse": RMSE,
+    "acc": ClassificationAccuracy,
+}
+
+
+# def load_metric(metric_class: typing.Union[str, Metric]):
+#     if metric_class in metric_class_mapping:
+#         return Metric(metric_class_mapping[metric_class])
+#     elif callable(metric_class):
+#         return Metric(metric_class)
