8819b449b90868c0a9f0d6b78976c7e591007204
aalok-sathe <asathe@mit.edu>
2022-06-02 13:03:20 -0400

    add a pereira2018 benchmark for MD

--- langbrainscore/benchmarks/__init__.py
+++ langbrainscore/benchmarks/__init__.py
@@ -7,6 +7,7 @@ from langbrainscore.benchmarks.pereira2018 import pereira2018_mean_froi
 
 supported_benchmarks: typing.Mapping[str, typing.Callable] = {
     "pereira2018_mean_froi_Lang": partial(pereira2018_mean_froi, network="Lang"),
+    "pereira2018_mean_froi_MD": partial(pereira2018_mean_froi, network="MD"),
     "pereira2018_mean_froi": pereira2018_mean_froi,
     "pereira2018_ind_voxels": NotImplemented,
 }
