6c1f81c6c86228bf775512337f1c4887e52f9124
dval <what@whatwhat.what>
2022-06-16 17:09:16 -0400

    trying to get logits out

--- brainscore_language/benchmarks/simple_next_word.py
+++ brainscore_language/benchmarks/simple_next_word.py
@@ -8,14 +8,38 @@ class SimpleNextWord(): #TODO create abstract benchmark interface
     def identifier(self):
         return self.benchmark_if
 
-def predict_next_word(input, tokenizer, model, **generator_args):
+def predict_next_word(input, tokenizer, model):
     seq = input
     print("\nInput sequence: ")
     print(seq)
 
-    input_ids = tokenizer(seq, return_tensors="pt")
+    inpts = tokenizer(seq, return_tensors="pt")
+    print("\nTokenized input data structure: ")
+    print(inpts)
 
-    res = model.generate(input_ids, **generator_args)
+    # res = model.generate(input_ids, **generator_args)
+    # output = tokenizer.batch_decode(res, skip_special_tokens=True)
 
-    output = tokenizer.batch_decode(res, skip_special_tokens=True)
-    print(output)
+    # inpt_ids = inpts["input_ids"]  # just IDS, no attn mask
+    # print("\nToken IDs and their words: ")
+    # for id in inpt_ids[0]:
+    #   word = tokenizer.decode(id)
+    #   print(id, word)
+
+    with torch.no_grad():
+        output = model(**inpts)
+    (loss, logits) = outputs[:2]
+
+    print("\nAll logits for next word: ")
+    print(logits)
+    print(logits.shape)
+
+    pred_id = torch.argmax(logits).item()
+    print("\nPredicted token ID of next word: ")
+    print(pred_id)
+
+    pred_word = tokenizer.decode(pred_id)
+    print("\nPredicted next word for sequence: ")
+    print(pred_word)
+
+    print("\nEnd demo ")
