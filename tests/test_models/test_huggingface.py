f02af67d845e069a5e69e7774c21a02744fabec7
dval <what@whatwhat.what>
2022-06-17 16:29:45 -0400

    PR clean up

--- tests/test_models/test_huggingface.py
+++ tests/test_models/test_huggingface.py
@@ -1,16 +1,6 @@
-# from brainscore_language.models.huggingface import distilgpt2
-# from brainscore_language.brainmodel import BrainModel
-
 from brainscore_language.models.huggingface import HuggingfaceModel
 
 def test_next_word():
-    # model = distilgpt2()
-    # model.start_task(task=BrainModel.Task.next_word)
-    # text = 'the quick brown fox'
-    # next_word = model.digest_text(text)
-    # assert isinstance(next_word, str)
-
-    # from brainscore_language.brainmodel import BrainModel
     temp = HuggingfaceModel(model_id='distilgpt2')
     print('Running', temp.identifier(), 'for next word prediction' )
     temp.start_task(BrainModel.Task.next_word)
