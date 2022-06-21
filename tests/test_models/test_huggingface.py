3fa6f2ab004ffa5a4215d099144b38fbe07b004d
dval <what@whatwhat.what>
2022-06-21 11:27:29 -0400

    PR updates

--- tests/test_models/test_huggingface.py
+++ tests/test_models/test_huggingface.py
@@ -1,13 +1,17 @@
 import unittest
 from brainscore_language.models.huggingface import HuggingfaceModel
 from brainmodel import BrainModel
+import logging
+logging.basicConfig(level=logging.INFO)
 
 class TestHuggingfaceModel(unittest.TestCase):
     def test_next_word(self):
         model = HuggingfaceModel(model_id='distilgpt2')
-        print('Running', model.identifier(), 'for next word prediction' )
+        # print('Running', model.identifier(), 'for next word prediction' )
+        # logging.info('This is an info message')
+        logging.info(' '.join(['Running', model.identifier(), 'for next word prediction']) )
         model.start_task(BrainModel.Task.next_word)
         text = 'the quick brown fox'
         next_word = model.digest_text(text)
         assert next_word == 'es'
-        print('next_word:', next_word)
+        logging.info(' '.join(['next_word:', next_word]) )
