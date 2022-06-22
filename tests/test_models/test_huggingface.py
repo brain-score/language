4ff5235d0e8e9dc275cb3b556cf5ce4f0e906bfc
dval <nuclearr.d@gmail.com>
2022-06-22 12:55:22 -0400

    draft HuggingFace model (#1)
    * trying to get something working depth-wise
    
    * trying to get logits out
    
    * trying to get logits out
    
    * first working prototype, needs cleanup
    
    * cleaning up
    
    * documenting predict_next_word
    
    * PR clean up
    
    * PR updates
    
    * PR updates
    
    * updating pyproject.toml
    
    * cleaning up
    
    * implemented BrainModel -> InSilicoModel class name change, and tokenizer/model in __init__
    
    * travis debug
    
    Co-authored-by: dval <what@whatwhat.what>

---
+++ tests/test_models/test_huggingface.py
@@ -0,0 +1,22 @@
+import unittest
+from brainscore_language.models.huggingface import HuggingfaceModel
+from brainmodel import InSilicoModel
+import logging
+logging.basicConfig(level=logging.INFO)
+
+class TestHuggingfaceModel(unittest.TestCase):
+    def test_next_word(self):
+        from transformers import AutoModelForCausalLM, AutoTokenizer
+        from transformers import logging as transformers_logging
+
+        model = HuggingfaceModel(model_id='distilgpt2',
+                                 model=AutoModelForCausalLM,
+                                 tokenizer=AutoTokenizer)
+        logging.info(' '.join(['Running', model.identifier(), 'for next word prediction']) )
+        model.start_task(InSilicoModel.Task.next_word)
+        text = 'the quick brown fox'
+        next_word = model.digest_text(text)
+        assert next_word == 'es'
+        logging.info( ' '.join(['next_word:', next_word]) )
+
+
