1dc467f599817784ba25cd6039ce4bf5026d1a28
Martin Schrimpf <m4rtinsch@gmail.com>
2022-06-22 16:54:50 -0400

    delete syspath modifications; format for pydoc

--- brainscore_language/models/huggingface.py
+++ brainscore_language/models/huggingface.py
@@ -1,33 +1,29 @@
-import sys
-import os.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-print(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-
 from brainmodel import InSilicoModel
-from benchmarks.predict_word_bag import predict_next_word
+
+from .predict_word_bag import predict_next_word
+
 
 class HuggingfaceModel(InSilicoModel):
-
     def __init__(
-        self,
-        model_id,
-        model,
-        tokenizer
+            self,
+            model_id: str,
+            model,
+            tokenizer
     ):
         """
-        Args:
-            model_id (str): the model id i.e. name
+        :param model_id: the model identifier / name
         """
 
         self.model_id = model_id
         self.model = model.from_pretrained(self.model_id)
         self.tokenizer = tokenizer.from_pretrained(self.model_id)
+        self.inference = None
 
     def identifier(self):
         return self.model_id
 
     def digest_text(self, todostimuli):
-        return self.inference(input = todostimuli,
+        return self.inference(input=todostimuli,
                               tokenizer=self.tokenizer,
                               model=self.model)
 
