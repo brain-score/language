94caeb9175bc9430241f60969b408d816af3f5e8
dval <nuclearr.d@gmail.com>
2022-06-23 14:15:36 -0400

    Dhaval/model interface (#2)
    * trying to get something working depth-wise
    
    * trying to get logits out
    
    * trying to get logits out
    
    * first working prototype, needs cleanup
    
    * cleaning up
    
    * documenting predict_next_word
    
    * PR clean up
    
    * PR updates
    
    * PR updates
    
    * updating pyproject.toml
    
    * cleaning up
    
    * implemented BrainModel -> InSilicoModel class name change, and tokenizer/model in __init__
    
    * travis debug
    
    * added default tokenizer
    
    * PR changes implemented
    
    Co-authored-by: dval <what@whatwhat.what>

--- tests/test_models/test_huggingface.py
+++ tests/test_models/test_huggingface.py
@@ -10,13 +10,13 @@ class TestHuggingfaceModel(unittest.TestCase):
         from transformers import logging as transformers_logging
 
         model = HuggingfaceModel(model_id='distilgpt2',
-                                 model=AutoModelForCausalLM,
-                                 tokenizer=AutoTokenizer)
+                                 model_class=AutoModelForCausalLM,
+                                 tokenizer_class=AutoTokenizer
+                                 )
+
         logging.info(' '.join(['Running', model.identifier(), 'for next word prediction']) )
         model.start_task(InSilicoModel.Task.next_word)
         text = 'the quick brown fox'
         next_word = model.digest_text(text)
         assert next_word == 'es'
         logging.info( ' '.join(['next_word:', next_word]) )
-
-
