802442fe263697cf08f69d869aa0f55205c0b699
dval <nuclearr.d@gmail.com>
2022-06-23 14:15:36 -0400

    Dhaval/model interface (#2)
    * trying to get something working depth-wise
    
    * trying to get logits out
    
    * trying to get logits out
    
    * first working prototype, needs cleanup
    
    * cleaning up
    
    * documenting predict_next_word
    
    * PR clean up
    
    * PR updates
    
    * PR updates
    
    * updating pyproject.toml
    
    * cleaning up
    
    * implemented BrainModel -> InSilicoModel class name change, and tokenizer/model in __init__
    
    * travis debug
    
    * added default tokenizer
    
    * PR changes implemented
    
    Co-authored-by: dval <what@whatwhat.what>

--- brainscore_language/models/huggingface.py
+++ brainscore_language/models/huggingface.py
@@ -6,22 +6,26 @@ print(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from brainmodel import InSilicoModel
 from benchmarks.predict_word_bag import predict_next_word
 
+from transformers import AutoModel, AutoTokenizer
+
 class HuggingfaceModel(InSilicoModel):
 
     def __init__(
         self,
         model_id,
-        model,
-        tokenizer
+        model_class=AutoModel,
+        tokenizer_class=AutoTokenizer
     ):
         """
         Args:
             model_id (str): the model id i.e. name
+            model (AutoModel): the model to run inference from e.g. from transformers import AutoModelForCausalLM
+            tokenizer (AutoTokenizer): the model's associated tokenizer
         """
 
         self.model_id = model_id
-        self.model = model.from_pretrained(self.model_id)
-        self.tokenizer = tokenizer.from_pretrained(self.model_id)
+        self.model = model_class.from_pretrained(self.model_id)
+        self.tokenizer = tokenizer_class.from_pretrained(self.model_id)
 
     def identifier(self):
         return self.model_id
