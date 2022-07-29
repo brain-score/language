613c229692d85bdfc074b3f4595534a870bcea4f
Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
2022-07-28 21:38:22 -0400

    add plugin unit tests

--- brainscore_language/plugins/test_plugins.py
+++ brainscore_language/plugins/test_plugins.py
@@ -10,19 +10,28 @@ class PluginTestRunner:
 	def __init__(self, plugin_directory):
 		self.plugin_directory = plugin_directory
 		self.plugin_name = str(self.plugin_directory).split('/')[-1]
-		self.has_requirements = (plugin_directory / 'requirements.txt').is_file()
+		self.has_requirements = (self.plugin_directory / 'requirements.txt').is_file()
+		self.conda_envs_dirpath = Path('/'.join(os.environ["CONDA_PREFIX"].split('/')[:-1]))
+		self.plugin_env_path = self.conda_envs_dirpath / self.plugin_name
 
 	def __call__(self):
+		self.validate_plugin()
 		self.run_tests()
 		self.teardown()
 
+	def validate_plugin(self):
+		assert (self.plugin_directory / 'test.py').is_file(), "'test.py' not found"
+
 	def run_tests(self):
-		subprocess.run(f"./brainscore_language/plugins/create_env.sh \
+		return subprocess.run(f"./brainscore_language/plugins/create_env.sh \
 			{self.plugin_directory} {self.plugin_name} \
 			{str(self.has_requirements).lower()}", shell=True)
 
 	def teardown(self):
-		subprocess.run(f"conda env remove -n {self.plugin_name}", shell=True)
+		completed_process = subprocess.run(f"conda env remove -n {self.plugin_name}", shell=True)
+		if completed_process.returncode is not 0:
+			subprocess.run(f"rm -rf {self.plugin_env_path}")
+		return completed_process
 
 
 if __name__ == '__main__':
@@ -30,6 +39,5 @@ if __name__ == '__main__':
 	# requires test file ("test.py")
 	for plugin_directory in PLUGINS_DIRPATH.glob('[!._]*'):
 		if plugin_directory.is_dir():
-			assert (plugin_directory / 'test.py').is_file(), "'test.py' not found"
 			plugin_test_runner = PluginTestRunner(plugin_directory)
 			plugin_test_runner()
---
+++ tests/test_test_plugins.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+import pytest
+import shutil
+import subprocess
+import time
+
+from brainscore_language.plugins.test_plugins import PluginTestRunner
+
+DUMMY_PLUGIN = "dummy_plugin"
+DUMMY_PLUGIN_PATH = Path(__file__).parent / DUMMY_PLUGIN
+DUMMY_TESTFILE = DUMMY_PLUGIN_PATH / "test.py"
+DUMMY_REQUIREMENTS = DUMMY_PLUGIN_PATH / "requirements.txt"
+
+
+class TestPluginTestRunner:
+
+	def setup_method(self):
+		DUMMY_PLUGIN_PATH.mkdir(parents=True, exist_ok=True)
+		Path(DUMMY_TESTFILE).touch()
+		Path(DUMMY_REQUIREMENTS).touch()
+
+	def teardown_method(self):
+		shutil.rmtree(DUMMY_PLUGIN_PATH)
+
+	def test_plugin_name(self):
+		plugin_test_runner = PluginTestRunner(DUMMY_PLUGIN_PATH)
+		assert plugin_test_runner.plugin_name == DUMMY_PLUGIN
+
+	def test_has_testfile(self):
+		DUMMY_TESTFILE.unlink()
+		plugin_test_runner = PluginTestRunner(DUMMY_PLUGIN_PATH)
+		with pytest.raises(Exception):
+			plugin_test_runner.validate_plugin()
+
+	def test_has_requirements(self):
+		DUMMY_REQUIREMENTS.unlink()
+		plugin_test_runner = PluginTestRunner(DUMMY_PLUGIN_PATH)
+		assert plugin_test_runner.has_requirements == False
+
+	def test_run_tests(self):
+		plugin_test_runner = PluginTestRunner(DUMMY_PLUGIN_PATH)
+		completed_process = plugin_test_runner.run_tests()
+		assert completed_process.returncode == 0
+
+	def test_teardown(self):
+		plugin_test_runner = PluginTestRunner(DUMMY_PLUGIN_PATH)
+		subprocess.run(f"conda create -n {DUMMY_PLUGIN} python=3.8 -y", shell=True)
+		assert plugin_test_runner.plugin_env_path.is_dir() == True
+		plugin_test_runner.teardown()
+		assert plugin_test_runner.plugin_env_path.is_dir() == False
+
+		
