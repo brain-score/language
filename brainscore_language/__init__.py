b2e083d45b0e72c9401e2bf7975252373281f043
Martin Schrimpf <m4rtinsch@gmail.com>
2022-08-01 19:24:21 -0400

    link to `Score` core documentation directly

--- brainscore_language/__init__.py
+++ brainscore_language/__init__.py
@@ -49,12 +49,14 @@ def score(model_identifier: str, benchmark_identifier: str) -> Score:
     so that the benchmark can interact with it.
     The benchmark will be looked up from the :data:`~brainscore_language.benchmarks` and evaluates the model on how
     brain-like it is under that benchmark's experimental paradigm, primate measurements, comparison metric, and ceiling
-    This results in a quantitative :class:`~brainscore_core.metrics.Score`
+    This results in a quantitative
+    :ref:`Score <https://brain-score-core.readthedocs.io/en/latest/modules/metrics.html#brainscore_core.metrics.Score>`
     ranging from 0 (least brain-like) to 1 (most brain-like under this benchmark).
 
     :param model_identifier: the identifier for the model
     :param benchmark_identifier: the identifier for the benchmark to test the model against
-    :return: a Score of how brain-like the candidate model is under this benchmark. The score is normalized by this benchmark’s ceiling such that 1 means the model matches the data to ceiling level.
+    :return: a Score of how brain-like the candidate model is under this benchmark. The score is normalized by
+        this benchmark's ceiling such that 1 means the model matches the data to ceiling level.
     """
     model: ArtificialSubject = load_model(model_identifier)
     benchmark: Benchmark = load_benchmark(benchmark_identifier)
