2feaf32f02cf7b7f343bea14682911d6ad4c7cc1
dhaval <1942909+d-val@users.noreply.github.com>
2022-08-01 15:37:48 -0400

    memory issues?

--- tests/test_models/test_huggingface.py
+++ tests/test_models/test_huggingface.py
@@ -63,45 +63,46 @@ class TestHuggingfaceSubject(unittest.TestCase):
         next_word = model.digest_text(text)['behavior'].values
         assert next_word == 'es'
 
-    # def test_representation_one_text_single_target(self):
-    #     """
-    #     This is a simple test that takes in text = 'the quick brown fox', and asserts that the `distilgpt2` layer
-    #     indexed by `representation_layer` has 1 text presentation and 768 neurons. This test is a stand-in prototype to
-    #     check if our model definitions are correct.
-    #     """
-    #     model = HuggingfaceSubject(model_id='distilgpt2', region_layer_mapping={
-    #         ArtificialSubject.RecordingTarget.language_system: 'transformer.h.0.ln_1'})
-    #     text = 'the quick brown fox'
-    #     logging.info(f'Running {model.identifier()} with text "{text}"')
-    #     model.perform_neural_recording(recording_target=ArtificialSubject.RecordingTarget.language_system,
-    #                                    recording_type=ArtificialSubject.RecordingType.spikerate_exact)
-    #     representations = model.digest_text(text)['neural']
-    #     assert len(representations['presentation']) == 1
-    #     assert representations['context'].squeeze() == text
-    #     assert len(representations['neuroid']) == 768
-    #     logging.info(f'representation shape is correct: {representations.shape}')
-    #
-    #
-    # def test_representation_one_text_two_targets(self):
-    #     model = HuggingfaceSubject(model_id='distilgpt2', region_layer_mapping={
-    #         ArtificialSubject.RecordingTarget.language_system_left_hemisphere: 'transformer.h.0.ln_1',
-    #         ArtificialSubject.RecordingTarget.language_system_right_hemisphere: 'transformer.h.1.ln_1'})
-    #     text = 'the quick brown fox'
-    #     logging.info(f'Running {model.identifier()} with text "{text}"')
-    #     model.perform_neural_recording(recording_target=ArtificialSubject.RecordingTarget.language_system_left_hemisphere,
-    #                                    recording_type=ArtificialSubject.RecordingType.spikerate_exact)
-    #     model.perform_neural_recording(recording_target=ArtificialSubject.RecordingTarget.language_system_right_hemisphere,
-    #                                    recording_type=ArtificialSubject.RecordingType.spikerate_exact)
-    #     representations = model.digest_text(text)['neural']
-    #     assert len(representations['presentation']) == 1
-    #     assert representations['context'].squeeze() == text
-    #     assert len(representations['neuroid']) == 768 * 2
-    #     assert set(representations['region'].values) == {
-    #         ArtificialSubject.RecordingTarget.language_system_left_hemisphere,
-    #         ArtificialSubject.RecordingTarget.language_system_right_hemisphere}
-    #     logging.info(f'representation shape is correct: {representations.shape}')
-    #
-    # # TODO: add test with long text input, e.g. thousands of words,
-    # #  to see if we need batching, and to stress-test token alignment
-    #
-    # # TODO: add test with multiple passage input and representation retrieval, e.g. ['the', 'quick brown', 'fox']
+    @pytest.mark.memory_intense
+    def test_representation_one_text_single_target(self):
+        """
+        This is a simple test that takes in text = 'the quick brown fox', and asserts that the `distilgpt2` layer
+        indexed by `representation_layer` has 1 text presentation and 768 neurons. This test is a stand-in prototype to
+        check if our model definitions are correct.
+        """
+        model = HuggingfaceSubject(model_id='distilgpt2', region_layer_mapping={
+            ArtificialSubject.RecordingTarget.language_system: 'transformer.h.0.ln_1'})
+        text = 'the quick brown fox'
+        logging.info(f'Running {model.identifier()} with text "{text}"')
+        model.perform_neural_recording(recording_target=ArtificialSubject.RecordingTarget.language_system,
+                                       recording_type=ArtificialSubject.RecordingType.spikerate_exact)
+        representations = model.digest_text(text)['neural']
+        assert len(representations['presentation']) == 1
+        assert representations['context'].squeeze() == text
+        assert len(representations['neuroid']) == 768
+        logging.info(f'representation shape is correct: {representations.shape}')
+
+    @pytest.mark.memory_intense
+    def test_representation_one_text_two_targets(self):
+        model = HuggingfaceSubject(model_id='distilgpt2', region_layer_mapping={
+            ArtificialSubject.RecordingTarget.language_system_left_hemisphere: 'transformer.h.0.ln_1',
+            ArtificialSubject.RecordingTarget.language_system_right_hemisphere: 'transformer.h.1.ln_1'})
+        text = 'the quick brown fox'
+        logging.info(f'Running {model.identifier()} with text "{text}"')
+        model.perform_neural_recording(recording_target=ArtificialSubject.RecordingTarget.language_system_left_hemisphere,
+                                       recording_type=ArtificialSubject.RecordingType.spikerate_exact)
+        model.perform_neural_recording(recording_target=ArtificialSubject.RecordingTarget.language_system_right_hemisphere,
+                                       recording_type=ArtificialSubject.RecordingType.spikerate_exact)
+        representations = model.digest_text(text)['neural']
+        assert len(representations['presentation']) == 1
+        assert representations['context'].squeeze() == text
+        assert len(representations['neuroid']) == 768 * 2
+        assert set(representations['region'].values) == {
+            ArtificialSubject.RecordingTarget.language_system_left_hemisphere,
+            ArtificialSubject.RecordingTarget.language_system_right_hemisphere}
+        logging.info(f'representation shape is correct: {representations.shape}')
+
+    # TODO: add test with long text input, e.g. thousands of words,
+    #  to see if we need batching, and to stress-test token alignment
+
+    # TODO: add test with multiple passage input and representation retrieval, e.g. ['the', 'quick brown', 'fox']
