20eafdbe312081eb6e47033d3385dd4b62972770
Martin Schrimpf <m4rtinsch@gmail.com>
2022-08-02 17:44:12 -0400

    implement split-halves ceiling normalization
    following https://github.com/brain-score/brain-score/blob/c51b8aa2c94212a9ac56c06c556afad0bb0a3521/brainscore/metrics/ceiling.py

--- brainscore_language/plugins/futrell2018/test.py
+++ brainscore_language/plugins/futrell2018/test.py
@@ -63,10 +63,15 @@ class TestBenchmark:
         reading_times = RandomState(0).random(10256)
         dummy_model = TestBenchmark.DummyModel(reading_times=reading_times)
         score = benchmark(dummy_model)
-        assert score == approx(0.0098731, abs=0.001)
+        assert score == approx(0.0098731 / .858, abs=0.001)
 
     def test_exact(self):
         benchmark = load_benchmark('Futrell2018-pearsonr')
         dummy_model = TestBenchmark.DummyModel(reading_times=benchmark.data.mean('subject').values)
         score = benchmark(dummy_model)
         assert score == approx(1)
+
+    def test_ceiling(self):
+        benchmark = load_benchmark('Futrell2018-pearsonr')
+        ceiling = benchmark.ceiling
+        assert ceiling == approx(.858, abs=.0005)
