7ba9c9b65d74fb99ace2d82a121886f2e0f15fad
Martin Schrimpf <m4rtinsch@gmail.com>
2022-08-01 21:13:31 -0400

    use sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'recommonmark',
     'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
 ]
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
@@ -58,7 +59,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
