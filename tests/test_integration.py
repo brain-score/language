659c1ae24ec7f79e1730b074af8ed6a8cfeca55d
Martin Schrimpf <m4rtinsch@gmail.com>
2022-08-02 17:48:24 -0400

    initial setup for model-benchmark integration tests

---
+++ tests/test_integration.py
@@ -0,0 +1,14 @@
+import pytest
+from pytest import approx
+
+from brainscore_language import load_model, load_benchmark
+
+
+@pytest.mark.parametrize('model_identifier, benchmark_identifier, expected_score', [
+    ('distilgpt2', 'Futrell2018-pearsonr', approx(.3, abs=.005)),
+])
+def test_model_benchmark(model_identifier, benchmark_identifier, expected_score):
+    model = load_model(model_identifier)
+    benchmark = load_benchmark(benchmark_identifier)
+    score = benchmark(model)
+    assert score == expected_score
