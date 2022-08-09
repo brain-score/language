7e83c5f2da49cc595d049e39684a2e3a1078209c
Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
2022-08-08 21:57:25 -0400

    stop plugin tests failing silently

--- brainscore_language/plugins/test_plugins.py
+++ brainscore_language/plugins/test_plugins.py
@@ -30,6 +30,7 @@ class PluginTestRunner:
 		completed_process = subprocess.run(f"./brainscore_language/plugins/create_env.sh \
 			{self.plugin_directory} {self.plugin_name} \
 			{str(self.has_requirements).lower()}", shell=True)
+		assert completed_process == 0
 		return completed_process
 
 	def teardown(self):
