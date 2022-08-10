a1d4e442a1c3013a658dc445b51c8a41e1d77d7a
Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
2022-08-09 22:01:20 -0400

    suppress plugin env setup logging

--- brainscore_language/plugins/create_env.sh
+++ brainscore_language/plugins/create_env.sh
@@ -9,13 +9,14 @@ PLUGIN_TEST_PATH=$PLUGIN_PATH/test.py
 echo $PLUGIN_NAME
 
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
-conda create -n $PLUGIN_NAME python=3.8 -y
+output=`conda create -n $PLUGIN_NAME python=3.8 -y 2>&1` || echo $output
 conda activate $PLUGIN_NAME
-echo $CONDA_PREFIX
+
 if $HAS_REQUIREMENTS; then
-	pip install -r $PLUGIN_REQUIREMENTS_PATH
+	output=`pip install -r $PLUGIN_REQUIREMENTS_PATH 2>&1` || echo $output
 else
 	echo "Warning: no requirements.txt found. Installing only base dependencies."
 fi
-python -m pip install -e ".[test]"
+
+output=`python -m pip install -e ".[test]" 2>&1` || echo $output
 pytest $PLUGIN_TEST_PATH
