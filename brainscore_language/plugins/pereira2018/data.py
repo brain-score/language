ff5e8c78edfc2bcbe107e49572be18b4f73e7dfd
Greta Tuckute <gretatu@mit.edu>
2022-08-16 16:39:33 -0400

    update bibtex and description

--- brainscore_language/plugins/pereira2018/data.py
+++ brainscore_language/plugins/pereira2018/data.py
@@ -6,24 +6,23 @@ from brainscore_language import datasets
 
 _logger = logging.getLogger(__name__)
 
-BIBTEX = """@proceedings{futrell2018natural,
-  title={The Natural Stories Corpus},
-  author={Futrell, Richard and Gibson, Edward and Tily, Harry J. and Blank, Idan and Vishnevetsky, Anastasia and
-          Piantadosi, Steven T. and Fedorenko, Evelina},
-  conference={International Conference on Language Resources and Evaluation (LREC)},
-  url={http://www.lrec-conf.org/proceedings/lrec2018/pdf/337.pdf},
-  year={2018}
+BIBTEX = """@article{Pereira2018TowardAU,
+  title={Toward a universal decoder of linguistic meaning from brain activation},
+  author={Francisco Pereira and Bin Lou and Brianna Pritchett and Samuel Ritter and Samuel J. Gershman and Nancy G. Kanwisher and Matthew M. Botvinick and Evelina Fedorenko},
+  journal={Nature Communications},
+  year={2018},
+  volume={9}
 }"""
 
 
 def load_from_s3():
     file_path = fetch_file(location_type="S3",
-                           location="https://brainscore-language.s3.amazonaws.com/assy_Futrell2018.nc",
+                           location="https://brainscore-language.s3.amazonaws.com/assy_Futrell2018.nc", # todo update paths
                            sha1="381ccc8038fbdb31235b5f3e1d350f359b5e287f")
     loader = AssemblyLoader(cls=BehavioralAssembly, file_path=file_path)
     assembly = loader.load()
-    assembly.attrs['identifier'] = "Futrell2018"
+    assembly.attrs['identifier'] = "Pereira2018ROI"
     return assembly
 
 
-datasets['Futrell2018'] = load_from_s3
+datasets['Pereira2018ROI'] = load_from_s3
