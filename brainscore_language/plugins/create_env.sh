2907cc548ae9449ae8f0cf44a04bc8dca13d27bd
Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
2022-08-21 15:16:01 -0400

    create envs only for extra dependencies

--- brainscore_language/plugins/create_env.sh
+++ brainscore_language/plugins/create_env.sh
@@ -8,14 +8,11 @@ PLUGIN_TEST_PATH=$PLUGIN_PATH/test.py
 
 echo $PLUGIN_NAME
 
-eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
-output=`conda create -n $PLUGIN_NAME python=3.8 -y 2>&1` || echo $output
-conda activate $PLUGIN_NAME
-
 if $HAS_REQUIREMENTS; then
+	eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+	output=`conda create -n $PLUGIN_NAME python=3.8 -y 2>&1` || echo $output
+	conda activate $PLUGIN_NAME
 	output=`pip install -r $PLUGIN_REQUIREMENTS_PATH 2>&1` || echo $output
-else
-	echo "Warning: no requirements.txt found. Installing only base dependencies."
 fi
 
 output=`python -m pip install -e ".[test]" 2>&1` || echo $output
