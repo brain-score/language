39f26ed446cefff45e346908c5a3a9aa638ba5da
Martin Schrimpf <mschrimpf@users.noreply.github.com>
2022-09-03 13:40:03 -0400

    add Glove with Gensim embedding wrapper (#59)
    * copy Glove implementation from neural-nlp
    
    https://github.com/mschrimpf/neural-nlp/blob/cedac1f868c8081ce6754ef0c13895ce8bc32efc/neural_nlp/models/implementations.py
    
    * adapt GloVe to ArtificialSubject using Gensim wrapper
    
    * automatically download weights
    
    * fix import
    
    * add tests README describing markers
    
    from https://github.com/brain-score/brain-score/blob/5e43c9495eb3de93bfc12ab6b2421c66ef4f4fc4/tests/README.md
    
    * mark glove test as travis_slow
    
    * remove redundant prints
    
    * use glove.txt directly, do away with word2vec conversion

---
+++ tests/test_model_helpers/test_embedding.py
@@ -0,0 +1,70 @@
+import logging
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+from brainscore_language.artificial_subject import ArtificialSubject
+from brainscore_language.model_helpers.embedding import GensimKeyedVectorsSubject
+
+logging.basicConfig(level=logging.INFO)
+
+
+class TestNeural:
+    @pytest.mark.parametrize('word, expected_vector', [
+        ('the', [-0.082752, 0.67204, -0.149879]),
+        ('quick', [1, 2, 3]),
+        ('brown', [3, 2, 1]),
+        ('fox', [8, 6, 1]),
+        ('jumped', [0, 2, 3]),
+        ('over', [0, 0, 0]),
+    ])
+    def test_by_word(self, word, expected_vector):
+        model = GensimKeyedVectorsSubject(
+            identifier='dummy', weights_file=Path(__file__).parent / 'mini_embeddings.word2vec', vector_size=3)
+        model.perform_neural_recording(recording_target=ArtificialSubject.RecordingTarget.language_system,
+                                       recording_type=ArtificialSubject.RecordingType.fMRI)
+        representations = model.digest_text(word)['neural']
+        assert len(representations['presentation']) == 1
+        assert representations['context'].squeeze() == word
+        assert len(representations['neuroid']) == 3
+
+    def test_multi_word(self):
+        model = GensimKeyedVectorsSubject(
+            identifier='dummy', weights_file=Path(__file__).parent / 'mini_embeddings.word2vec', vector_size=3)
+        text = 'the quick brown fox'
+        model.perform_neural_recording(recording_target=ArtificialSubject.RecordingTarget.language_system,
+                                       recording_type=ArtificialSubject.RecordingType.fMRI)
+        representations = model.digest_text(text)['neural']
+        assert len(representations['presentation']) == 1
+        assert representations['context'].squeeze() == text
+        assert len(representations['neuroid']) == 3
+
+    def test_list_input(self):
+        model = GensimKeyedVectorsSubject(
+            identifier='dummy', weights_file=Path(__file__).parent / 'mini_embeddings.word2vec', vector_size=3)
+        text = ['the quick', 'brown fox', 'jumps over', 'the lazy dog']
+        model.perform_neural_recording(recording_target=ArtificialSubject.RecordingTarget.language_system,
+                                       recording_type=ArtificialSubject.RecordingType.fMRI)
+        representations = model.digest_text(text)['neural']
+        assert len(representations['presentation']) == 4
+        np.testing.assert_array_equal(representations['context'], text)
+        assert len(representations['neuroid']) == 3
+
+    def test_one_text_two_targets(self):
+        model = GensimKeyedVectorsSubject(
+            identifier='dummy', weights_file=Path(__file__).parent / 'mini_embeddings.word2vec', vector_size=3)
+        text = 'the quick brown fox'
+        model.perform_neural_recording(
+            recording_target=ArtificialSubject.RecordingTarget.language_system_left_hemisphere,
+            recording_type=ArtificialSubject.RecordingType.fMRI)
+        model.perform_neural_recording(
+            recording_target=ArtificialSubject.RecordingTarget.language_system_right_hemisphere,
+            recording_type=ArtificialSubject.RecordingType.fMRI)
+        representations = model.digest_text(text)['neural']
+        assert len(representations['presentation']) == 1
+        assert representations['context'].squeeze() == text
+        assert len(representations['neuroid']) == 3 * 2
+        assert set(representations['recording_target'].values) == {
+            ArtificialSubject.RecordingTarget.language_system_left_hemisphere,
+            ArtificialSubject.RecordingTarget.language_system_right_hemisphere}
