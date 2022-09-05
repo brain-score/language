ea4b631178d327641b3e093f893df183f02239da
Martin Schrimpf <m4rtinsch@gmail.com>
2022-09-05 07:03:30 -0400

    mark as slow; update score

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,8 +4,9 @@ from pytest import approx
 from brainscore_language import load_model, load_benchmark
 
 
+@pytest.mark.travis_slow
 @pytest.mark.parametrize('model_identifier, benchmark_identifier, expected_score', [
-    ('distilgpt2', 'Futrell2018-pearsonr', approx(.3, abs=.005)),
+    ('distilgpt2', 'Futrell2018-pearsonr', approx(.01988352, abs=.005)),
 ])
 def test_model_benchmark(model_identifier, benchmark_identifier, expected_score):
     model = load_model(model_identifier)
