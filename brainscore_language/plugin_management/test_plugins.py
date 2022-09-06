50ca79248324da9dc01e94f7ce4a9cfcb5131b07
kvfairchild <kvg0@mit.edu>
2022-09-06 12:31:30 -0400

    fail travis build on error for any plugin (#60)
    * test dummy failure in pereira
    
    * typo fix
    
    * collect results from all plugin tests
    
    * add results list
    
    * Remove dummy error
    
    * POSIX compliance (add EOF blank line)
    
    * relative import
    
    * Update brainscore_language/plugin_management/test_plugins.py
    
    Co-authored-by: Martin Schrimpf <mschrimpf@users.noreply.github.com>
    
    * absolute import
    
    * ignore flagged tests
    
    * update plugin results
    
    * set errors to exit status 1
    
    * cleanup
    
    Co-authored-by: Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
    Co-authored-by: Martin Schrimpf <mschrimpf@users.noreply.github.com>

--- brainscore_language/plugin_management/test_plugins.py
+++ brainscore_language/plugin_management/test_plugins.py
@@ -1,3 +1,4 @@
+import pytest_check as check
 import shutil
 import subprocess
 import warnings
@@ -7,12 +8,13 @@ PLUGIN_TYPES = ['benchmarks', 'data', 'metrics', 'models']
 
 
 class PluginTestRunner:
-    def __init__(self, plugin_directory):
+    def __init__(self, plugin_directory, results):
         self.plugin_directory = plugin_directory
         self.plugin_type = Path(self.plugin_directory).parent.name
         self.plugin_name = self.plugin_type + '_' + Path(self.plugin_directory).name
         self.plugin_env_path = Path(self.get_conda_base()) / 'envs' / self.plugin_name
         self.has_requirements = (self.plugin_directory / 'requirements.txt').is_file()
+        self.results = results
 
     def __call__(self):
         self.validate_plugin()
@@ -29,6 +31,9 @@ class PluginTestRunner:
         completed_process = subprocess.run(f"bash {Path(__file__).parent}/run_plugin.sh \
 			{self.plugin_directory} {self.plugin_name} \
 			{str(self.has_requirements).lower()}", shell=True)
+        check.equal(completed_process.returncode, 0)
+        self.results[self.plugin_name] = (completed_process.returncode)
+
         return completed_process
 
     def teardown(self):
@@ -37,7 +42,7 @@ class PluginTestRunner:
         if completed_process.returncode != 0:  # directly remove env dir if conda fails
             try:
                 shutil.rmtree(self.plugin_env_path)
-                completed_process = 0
+                completed_process.returncode = 0
             except Exception as e:
                 warnings.warn(f"conda env {self.plugin_name} removal failed and must be manually deleted.")
         return completed_process
@@ -46,9 +51,15 @@ class PluginTestRunner:
 if __name__ == '__main__':
     # run tests for each plugin
     # requires test file ("test.py")
+    results = {}
+
     for plugin_type in PLUGIN_TYPES:
         plugins_dir = Path(Path(__file__).parents[1], plugin_type)
         for plugin in plugins_dir.glob('[!._]*'):
             if plugin.is_dir():
-                plugin_test_runner = PluginTestRunner(plugin)
+                plugin_test_runner = PluginTestRunner(plugin, results)
                 plugin_test_runner()
+
+    plugins_with_errors = {k:v for k,v in results.items() if v == 1}
+    num_plugins_failed = len(plugins_with_errors)
+    assert num_plugins_failed == 0, f"\n{num_plugins_failed} plugin tests failed\n{plugins_with_errors}"
