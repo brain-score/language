93fc15b5eaa3660e3edbff40943f2c4b50933f9e
Martin Schrimpf <m4rtinsch@gmail.com>
2022-09-14 14:41:04 -0400

    use `score` method; add Pereira2018

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,16 +1,15 @@
 import pytest
 from pytest import approx
 
-from brainscore_language import load_model, load_benchmark
+from brainscore_language import score
 
 
 @pytest.mark.travis_slow
 @pytest.mark.parametrize('model_identifier, benchmark_identifier, expected_score', [
     ('distilgpt2', 'Futrell2018-pearsonr', approx(0.36144805, abs=.0005)),
+    ('distilgpt2', 'Pereira2018.243sentences-linear', approx(0.72309996, abs=.0005)),
     ('gpt2-xl', 'Futrell2018-pearsonr', approx(0.31825621, abs=.0005)),
 ])
-def test_model_benchmark(model_identifier, benchmark_identifier, expected_score):
-    model = load_model(model_identifier)
-    benchmark = load_benchmark(benchmark_identifier)
-    score = benchmark(model)
-    assert score == expected_score
+def test_score(model_identifier, benchmark_identifier, expected_score):
+    actual_score = score(model_identifier=model_identifier, benchmark_identifier=benchmark_identifier)
+    assert actual_score == expected_score
