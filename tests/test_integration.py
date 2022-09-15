fa60ad44c45c27ddd8fbbb9f42df6eff03e86611
Martin Schrimpf <m4rtinsch@gmail.com>
2022-09-15 10:50:02 -0400

    add Glove on Pereira2018

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -8,6 +8,7 @@ from brainscore_language import score
 @pytest.mark.parametrize('model_identifier, benchmark_identifier, expected_score', [
     ('distilgpt2', 'Futrell2018-pearsonr', approx(0.36144805, abs=.0005)),
     ('distilgpt2', 'Pereira2018.243sentences-linear', approx(0.72309996, abs=.0005)),
+    ('glove-840b', 'Pereira2018.384sentences-linear', approx(0.21466593, abs=.0005)),
     ('gpt2-xl', 'Futrell2018-pearsonr', approx(0.31825621, abs=.0005)),
 ])
 def test_score(model_identifier, benchmark_identifier, expected_score):
