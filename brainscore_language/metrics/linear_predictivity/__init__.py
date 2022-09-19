da038f31f5a6891a358528964eef95cef8b8dc82
Greta Tuckute <gretatu@mit.edu>
2022-09-19 13:43:12 -0400

    update metric init

--- brainscore_language/metrics/linear_predictivity/__init__.py
+++ brainscore_language/metrics/linear_predictivity/__init__.py
@@ -171,10 +171,10 @@ def pearsonr_correlation(xarray_kwargs=None):
     return XarrayCorrelation(scipy.stats.pearsonr, **xarray_kwargs)
 
 
-def linear_pearsonr(*args, regression_kwargs=None, correlation_kwargs=None, **kwargs):
+def linreg_pearsonr(*args, regression_kwargs=None, correlation_kwargs=None, **kwargs):
     regression = linear_regression(regression_kwargs or {})
     correlation = pearsonr_correlation(correlation_kwargs or {})
     return CrossRegressedCorrelation(*args, regression=regression, correlation=correlation, **kwargs)
 
 
-metric_registry['linear_pearsonr'] = linear_pearsonr
+metric_registry['linreg_pearsonr'] = linreg_pearsonr
