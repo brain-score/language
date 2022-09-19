59c734a5af34b27c3e80419b38d89184f8eccb40
aalok-sathe <asathe@mit.edu>
2022-09-19 15:33:02 -0400

    scripts for packaging remodeled fROI-level data from pereira2018

---
+++ brainscore_language/data/pereira2018_v2022/__init__.py
@@ -0,0 +1,23 @@
+import logging
+
+from brainscore_language import data_registry
+
+# from brainscore_language.utils.s3 import load_from_s3
+from brainscore_language.utils.local import load_from_disk
+
+_logger = logging.getLogger(__name__)
+
+BIBTEX = """@article{pereira2018toward,
+  title={Toward a universal decoder of linguistic meaning from brain activation},
+  author={Pereira, Francisco and Lou, Bin and Pritchett, Brianna and Ritter, Samuel and Gershman, Samuel J 
+          and Kanwisher, Nancy and Botvinick, Matthew and Fedorenko, Evelina},
+  journal={Nature communications},
+  volume={9},
+  number={1},
+  pages={1--13},
+  year={2018},
+  publisher={Nature Publishing Group}
+}"""
+
+
+data_registry["Pereira2018_v2022.language"] = lambda: load_from_disk("Pereira2018ROI")
