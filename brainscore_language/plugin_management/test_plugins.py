e73a34a3dc0363ea0a1d5ffd9dab29f6d480ddd3
Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
2022-09-26 02:13:14 -0400

    option to run single test

--- brainscore_language/plugin_management/test_plugins.py
+++ brainscore_language/plugin_management/test_plugins.py
@@ -1,3 +1,4 @@
+import argparse
 import pytest_check as check
 import shutil
 import subprocess
@@ -8,12 +9,13 @@ PLUGIN_TYPES = ['benchmarks', 'data', 'metrics', 'models']
 
 
 class PluginTestRunner:
-    def __init__(self, plugin_directory, results):
+    def __init__(self, plugin_directory, results, test=False):
         self.plugin_directory = plugin_directory
         self.plugin_type = Path(self.plugin_directory).parent.name
         self.plugin_name = self.plugin_type + '_' + Path(self.plugin_directory).name
         self.plugin_env_path = Path(self.get_conda_base()) / 'envs' / self.plugin_name
         self.has_requirements = (self.plugin_directory / 'requirements.txt').is_file()
+        self.test = test if test else False
         self.results = results
 
     def __call__(self):
@@ -30,7 +32,7 @@ class PluginTestRunner:
     def run_tests(self):
         completed_process = subprocess.run(f"bash {Path(__file__).parent}/run_plugin.sh \
 			{self.plugin_directory} {self.plugin_name} \
-			{str(self.has_requirements).lower()}", shell=True)
+			{str(self.has_requirements).lower()} {self.test}", shell=True)
         check.equal(completed_process.returncode, 0)
         self.results[self.plugin_name] = (completed_process.returncode)
 
@@ -47,19 +49,40 @@ class PluginTestRunner:
                 warnings.warn(f"conda env {self.plugin_name} removal failed and must be manually deleted.")
         return completed_process
 
+def arg_parser():
+    parser = argparse.ArgumentParser(description='Run single specified test or tests for each plugin')
+    parser.add_argument('test_file', type=str, nargs='?',help='Optional: full path of test file')
+    parser.add_argument('--test', type=str, help='Optional: name of test to run', required=False)
+    args = parser.parse_args()
+
+    return args
+
 
 if __name__ == '__main__':
-    # run tests for each plugin
     # requires test file ("test.py")
     results = {}
 
-    for plugin_type in PLUGIN_TYPES:
+    args = arg_parser()
+    if args.test_file and Path(args.test_file).exists():
+        filename = args.test_file.split('/')[-1]
+        plugin_dirname = args.test_file.split('/')[-2]
+        plugin_type = args.test_file.split('/')[-3]
         plugins_dir = Path(Path(__file__).parents[1], plugin_type)
-        for plugin in plugins_dir.glob('[!._]*'):
-            if plugin.is_dir():
-                plugin_test_runner = PluginTestRunner(plugin, results)
-                plugin_test_runner()
+        plugin = plugins_dir / plugin_dirname
+        assert filename == "test.py", "Filepath not recognized as test file, must be 'test.py'."
+        assert plugin_type in PLUGIN_TYPES, "Filepath not recognized as plugin test file."
+        plugin_test_runner = PluginTestRunner(plugin, results, test=args.test)
+        plugin_test_runner()
+    elif not args.file:
+        for plugin_type in PLUGIN_TYPES:
+            plugins_dir = Path(Path(__file__).parents[1], plugin_type)
+            for plugin in plugins_dir.glob('[!._]*'):
+                if plugin.is_dir():
+                    plugin_test_runner = PluginTestRunner(plugin, results)
+                    plugin_test_runner()
+    else:
+        warnings.warn("Filepath does not exist or is not recognized as plugin test file.")
 
-    plugins_with_errors = {k:v for k,v in results.items() if v == 1}
-    num_plugins_failed = len(plugins_with_errors)
-    assert num_plugins_failed == 0, f"\n{num_plugins_failed} plugin tests failed\n{plugins_with_errors}"
+    # plugins_with_errors = {k:v for k,v in results.items() if v == 1}
+    # num_plugins_failed = len(plugins_with_errors)
+    # assert num_plugins_failed == 0, f"\n{num_plugins_failed} plugin tests failed\n{plugins_with_errors}"
