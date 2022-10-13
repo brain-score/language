b409500a55138a9d59fabfd9b0025684044fb935
Martin Schrimpf <mschrimpf@users.noreply.github.com>
2022-10-13 16:42:42 -0400

    print score rather than return; add command line test (#77)
    * print score rather than return; add command line test
    
    * add fire dependency
    
    * mark cli test as slow

--- brainscore_language/__main__.py
+++ brainscore_language/__main__.py
@@ -1,7 +1,12 @@
 import fire
 
-# noinspection PyUnresolvedReferences
-from brainscore_language import score
+from brainscore_language import score as _score_function
+
+
+def score(model_identifier: str, benchmark_identifier: str):
+    result = _score_function(model_identifier, benchmark_identifier)
+    print(result)  # print instead of return because fire has issues with xarray objects
+
 
 if __name__ == '__main__':
     fire.Fire()
