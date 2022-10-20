e86fb80cfd6430b3542f9bc57229e0039c1b8b05
Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
2022-10-19 23:48:54 -0400

    add not in keys assertion

--- tests/test_plugin_management/test_import_plugin.py
+++ tests/test_plugin_management/test_import_plugin.py
@@ -6,12 +6,12 @@ from brainscore_language.plugin_management.import_plugin import ImportPlugin
 
 def test_import_plugin():
 	os.environ['BSL_DEPENDENCY_INSTALL'] = os.getenv('BSL_DEPENDENCY_INSTALL', 'newenv')
-	assert len(benchmark_registry) == 0
-	ImportPlugin('benchmarks', 'Futrell2018-pearsonr')
-	assert 'Futrell2018-pearsonr' in benchmark_registry.keys()
+	assert 'Wikitext-accuracy' not in benchmark_registry.keys()
+	ImportPlugin('benchmarks', 'Wikitext-accuracy')
+	assert 'Wikitext-accuracy' in benchmark_registry.keys()
 
 def test_import_plugin_with_requirements():
 	os.environ['BSL_DEPENDENCY_INSTALL'] = os.getenv('BSL_DEPENDENCY_INSTALL', 'newenv')
-	assert len(metric_registry) == 0
-	ImportPlugin('metrics', 'linear_pearsonr')
-	assert 'linear_pearsonr' in metric_registry.keys()
+	assert 'pearsonr' not in benchmark_registry.keys()
+	ImportPlugin('metrics', 'pearsonr')
+	assert 'pearsonr' in metric_registry.keys()
