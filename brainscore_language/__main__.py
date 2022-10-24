1f8b2087729c8f4cb237ef0e3cff905788bcc755
Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
2022-10-24 18:59:28 -0400

    nit

--- brainscore_language/__main__.py
+++ brainscore_language/__main__.py
@@ -4,10 +4,10 @@ from brainscore_language import score as _score_function
 
 
 def score(model_identifier: str, benchmark_identifier: str):
-    
     result = _score_function(model_identifier, benchmark_identifier)
     print(result)  # print instead of return because fire has issues with xarray objects
 
 
 if __name__ == '__main__':
     fire.Fire()
+    
