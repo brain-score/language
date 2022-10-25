69db25f1a046b378ba5648748db4b7a9f3751179
Martin Schrimpf <m4rtinsch@gmail.com>
2022-10-25 08:23:02 -0400

    mark lm1b tests as memory intense
    does not load on travis: https://app.travis-ci.com/github/brain-score/language/jobs/586558185

--- brainscore_language/models/lm1b/test.py
+++ brainscore_language/models/lm1b/test.py
@@ -1,13 +1,14 @@
 import numpy as np
+import pytest
 
 from brainscore_language import load_model
 from brainscore_language.artificial_subject import ArtificialSubject
-import pytest
 
 
+@pytest.mark.memory_intense
 @pytest.mark.parametrize('model_identifier, expected_reading_times', [
-    ('lm1b', [28.3291111 , 17.69329643, 15.81945515, 15.53895187, 11.78036213,
-        4.01630831,  1.68043554, 14.99522114]),
+    ('lm1b', [28.3291111, 17.69329643, 15.81945515, 15.53895187, 11.78036213,
+              4.01630831, 1.68043554, 14.99522114]),
 ])
 def test_reading_times(model_identifier, expected_reading_times):
     model = load_model(model_identifier)
@@ -17,6 +18,7 @@ def test_reading_times(model_identifier, expected_reading_times):
     np.testing.assert_allclose(reading_times, expected_reading_times, atol=0.0001)
 
 
+@pytest.mark.memory_intense
 @pytest.mark.parametrize('model_identifier, expected_next_words', [
     ('lm1b', ['has', 'scene', 'House', ',', 'and', 'of']),
 ])
@@ -28,6 +30,7 @@ def test_next_word(model_identifier, expected_next_words):
     np.testing.assert_array_equal(next_word_predictions, expected_next_words)
 
 
+@pytest.mark.memory_intense
 @pytest.mark.parametrize('model_identifier, feature_size', [
     ('lm1b', 1024),
 ])
@@ -35,7 +38,7 @@ def test_neural(model_identifier, feature_size):
     model = load_model(model_identifier)
     text = ['the', 'quick brown fox', 'jumps over', 'the lazy dog']
     model.start_neural_recording(recording_target=ArtificialSubject.RecordingTarget.language_system,
-                                   recording_type=ArtificialSubject.RecordingType.fMRI)
+                                 recording_type=ArtificialSubject.RecordingType.fMRI)
     representations = model.digest_text(text)['neural']
     assert len(representations['presentation']) == len(text)
     np.testing.assert_array_equal(representations['stimulus'], text)
