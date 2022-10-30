6f7d9073ed0f4e337ddacaa9de8ac4d13c752fac
Martin Schrimpf <mschrimpf@users.noreply.github.com>
2022-10-29 21:49:44 -0400

    format test parametrization (#98)

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -12,21 +12,14 @@ from brainscore_language import score
     "model_identifier, benchmark_identifier, expected_score",
     [
         ("distilgpt2", "Futrell2018-pearsonr", approx(0.36144805, abs=0.0005)),
-        (
-            "distilgpt2",
-            "Pereira2018.243sentences-linear",
-            approx(0.72309996, abs=0.0005),
-        ),
-        (
-            "glove-840b",
-            "Pereira2018.384sentences-linear",
-            approx(0.18385368, abs=0.0005),
-        ),
+        ("distilgpt2", "Pereira2018.243sentences-linear", approx(0.72309996, abs=0.0005)),
+        ("glove-840b", "Pereira2018.384sentences-linear", approx(0.18385368, abs=0.0005)),
         ("gpt2-xl", "Futrell2018-pearsonr", approx(0.31825621, abs=0.0005)),
     ],
 )
 def test_score(model_identifier, benchmark_identifier, expected_score):
-    actual_score = score(model_identifier=model_identifier, benchmark_identifier=benchmark_identifier, install_dependencies="newenv")
+    actual_score = score(model_identifier=model_identifier, benchmark_identifier=benchmark_identifier,
+                         install_dependencies="newenv")
     assert actual_score == expected_score
 
 
