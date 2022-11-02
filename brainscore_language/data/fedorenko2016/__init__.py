b2f17c4fcfbeb5451faaac59dcb0dc4d08b822b9
Martin Schrimpf <m4rtinsch@gmail.com>
2022-11-02 15:19:26 -0400

    fix newline

--- brainscore_language/data/fedorenko2016/__init__.py
+++ brainscore_language/data/fedorenko2016/__init__.py
@@ -16,7 +16,6 @@ BIBTEX = """@article{fedorenko2016neural,
   year={2016},
   publisher={National Acad Sciences}
 }"""
-
 data_registry['Fedorenko2016.language'] = lambda: load_from_s3(
     identifier="Fedorenko2016.language",
     version_id="qvB7YZfEjbXEE64bODNLlQlZKWGpgPhy",
