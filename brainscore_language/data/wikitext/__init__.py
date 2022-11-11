340e1e030e9c64ea00dc88115c7962b0b4e9bf20
Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
2022-11-10 19:50:59 -0500

    add test

--- brainscore_language/data/wikitext/__init__.py
+++ brainscore_language/data/wikitext/__init__.py
@@ -8,5 +8,15 @@ def wikitext2TestFromHuggingface():
     lines = dataset['text']
     return lines
 
+def miniWikitext2TestFromHuggingface():
+    dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split='test')
+    lines = dataset['text']
+    print(len(lines))
+    print(lines[100:115])
+    # return lines
+
 
 data_registry['wikitext-2/test'] = wikitext2TestFromHuggingface
+
+if __name__ == '__main__':
+    miniWikitext2TestFromHuggingface()
