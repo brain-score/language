60000d6bdf2e8accd49c9b6f4e2f719e6460ed41
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-11-30 13:19:56 -0500

    fix in huggingface model for extracting layer activation (#110)
    * fix in huggingface model for extracting layer activation
    
    * simplify output item
    
    Co-authored-by: Martin Schrimpf <mschrimpf@users.noreply.github.com>

--- brainscore_language/model_helpers/huggingface.py
+++ brainscore_language/model_helpers/huggingface.py
@@ -246,7 +246,10 @@ class HuggingfaceSubject(ArtificialSubject):
                        target_dict: dict) -> RemovableHandle:
         # instantiate parameters to function defaults; otherwise they would change on next function call
         def hook_function(_layer: torch.nn.Module, _input, output: torch.Tensor, key=key):
-            target_dict[key] = self._tensor_to_numpy(output)
+            # fix for when taking out only the hidden state, this is different from dropout because of residual state
+            # see:  https://github.com/huggingface/transformers/blob/c06d55564740ebdaaf866ffbbbabf8843b34df4b/src/transformers/models/gpt2/modeling_gpt2.py#L428
+            output = output[0] if len(output) > 1 else output
+            target_dict[key] = output
 
         hook = layer.register_forward_hook(hook_function)
         return hook
