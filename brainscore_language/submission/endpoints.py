a0d690dc8cf68f71a7df9d8f2585179336366135
Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
2023-01-31 14:54:12 -0500

    move named args

--- brainscore_language/submission/endpoints.py
+++ brainscore_language/submission/endpoints.py
@@ -19,7 +19,8 @@ class LanguagePlugins(DomainPlugins):
 
 
 language_plugins = LanguagePlugins()
-run_scoring_endpoint = RunScoringEndpoint(language_plugins, db_secret=config.get_database_secret())
+# run_scoring_endpoint = RunScoringEndpoint(language_plugins, db_secret='placeholder')
+# run_scoring_endpoint = RunScoringEndpoint(language_plugins, db_secret=config.get_database_secret())
 
 
 def _get_ids(args_dict: Dict[str, Union[str, List]], key: str) -> Union[List, str, None]:
@@ -28,34 +29,32 @@ def _get_ids(args_dict: Dict[str, Union[str, List]], key: str) -> Union[List, st
 
 def run_scoring(args_dict: Dict[str, Union[str, List]]):
     """ prepares `args_dict` as parameters for the `run_scoring_endpoint`. """
+    print(args_dict)
+
     new_models = _get_ids(args_dict, 'new_models')
     new_benchmarks = _get_ids(args_dict, 'new_benchmarks')
 
-    if new_models and new_benchmarks:
-        args_dict['models'] = RunScoringEndpoint.ALL_PUBLIC
-        args_dict['benchmarks'] = RunScoringEndpoint.ALL_PUBLIC
-    elif new_benchmarks:
-        args_dict['models'] = RunScoringEndpoint.ALL_PUBLIC
-        args_dict['benchmarks'] = new_benchmarks
-    elif new_models:
-        args_dict['models'] = new_models
-        args_dict['benchmarks'] = RunScoringEndpoint.ALL_PUBLIC
+    # if new_models and new_benchmarks:
+    #     args_dict['models'] = RunScoringEndpoint.ALL_PUBLIC
+    #     args_dict['benchmarks'] = RunScoringEndpoint.ALL_PUBLIC
+    # elif new_benchmarks:
+    #     args_dict['models'] = RunScoringEndpoint.ALL_PUBLIC
+    #     args_dict['benchmarks'] = new_benchmarks
+    # elif new_models:
+    #     args_dict['models'] = new_models
+    #     args_dict['benchmarks'] = RunScoringEndpoint.ALL_PUBLIC
 
-    remove_keys = ['new_benchmarks', 'new_models']
-    new_args = {k: v for k, v in args_dict.items() if k not in remove_keys}  # preserve other keys, e.g. `run_score`
-    print(new_args)
+    # remove_keys = ['new_benchmarks', 'new_models']
+    # new_args = {k: v for k, v in args_dict.items() if k not in remove_keys}  # preserve other keys, e.g. `run_score`
+    # print(new_args)
 
-    run_scoring_endpoint(**new_args)
+    # run_scoring_endpoint(**new_args)
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('jenkins_id', type=int,
                         help='The id of the current jenkins run')
-    parser.add_argument('--new_models', type=str, nargs='*', default=None,
-                        help='The identifiers of newly submitted models to score on all benchmarks')
-    parser.add_argument('--new_benchmarks', type=str, nargs='*', default=None,
-                        help='The identifiers of newly submitted benchmarks on which to score all models')
     parser.add_argument('user_id', type=int, nargs='?', default=2,
                         help='ID of submitting user in the postgres DB')
     parser.add_argument('model_type', type=str, nargs='?', default='artificialsubject',
@@ -64,6 +63,10 @@ def parse_args() -> argparse.Namespace:
                         help='Public (or private) submission?')
     parser.add_argument('competition', type=str, nargs='?', default=None,
                         help='Name of competition for which submission is being scored')
+    parser.add_argument('--new_models', type=str, nargs='*', default=None,
+                        help='The identifiers of newly submitted models to score on all benchmarks')
+    parser.add_argument('--new_benchmarks', type=str, nargs='*', default=None,
+                        help='The identifiers of newly submitted benchmarks on which to score all models')
     args, remaining_args = parser.parse_known_args()
 
     return args
