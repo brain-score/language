466f8c32e4f948335d09ef7fd4c17ccdf20ff990
Katherine Fairchild <kvfairchild@gmail.com>
2023-03-30 14:33:17 -0400

    email web submitter on test failure

--- .github/workflows/call_jenkins.py
+++
@@ -1,9 +0,0 @@
-import ast
-import sys
-
-from brainscore_core.submission.endpoints import process_github_submission
-
-
-if __name__ == '__main__':
-	args_dict = ast.literal_eval(sys.argv[1])
-	process_github_submission(args_dict)
