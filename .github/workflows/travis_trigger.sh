brain-score/language

commit 1dab959ca7d9f5646fc78a67f2bd78fd7620e6f5

Automatically approve, merge, and score PRs containing only changes to plugins (#108)
* fix newline

* setup run_scoring endpoint for language

* track submission branch of core

* jenkins to run_scoring

* set brainscore_core to track kvf/pr-to-score

* add submission workflows

* trigger scoring run

* remove on check_suite

* typo

* move parallelization out of jenkins call and into run_scoring

* cleanup

* cleanup

* type hints

* move model_helpers dependencies

* handle 0-dim tensors

* format

* format

* resolve merge conflicts

* only run on travis success

* fix tab -> spaces

* fix output type

* resolve `ALL_PUBLIC` models or benchmarks in core

* remove 'all_[plugin_type]s'

* move named args

* move named args

* debug

* debug

* add domain

* add conda_active arg to score

* fix infinite loop

* make conda_active default in init

* documentation

* split out automerge from scoring

* update documentation

* update documentation

* update documentation

* update documentation

* remove unnecessary steps

* trigger scoring of new/updated models or benchmarks on merge to main

* user_id -> author_email

* split automerge & score into two files

* call process_github_submission directly

* add web-specific automerge label

* combine getting author email & bs uid

* add author_email to args

* get user id from email

* get user id from email

* move documentation into README

* move documentation into README

* move documentation into README

* move documentation into yaml

* add names to steps

* remove automerge during testing

* remove comment

* typo fix

* get email from uid, create user if no uid exists

* email web submitter on test failure

* change call_jenkins to call_endpoints

* update link in failure email

* clarify trigger language

* reorder PLUGIN_DIRS

.github/workflows/automerge_plugin-only_prs.yml
@@ -1,0 +1,179 @@
+name: Automatically merge plugin-only PRs
+
+
+# Triggered on all PRs either by
+# - completion of CI checks, OR
+# - tagging with label
+# 1) If PR is labeled "automerge" or "automerge-web" 
+# (all website submissions are tagged "automerge-web"),
+# checks if Travis tests pass. If yes, THEN
+# 2) Checks if PR modifies any code outside plugin dirs. 
+# If no changes are made beyond new or revised plugins
+# (subdirs of /benchmarks, /data, /models, or /metrics) 
+# the PR is automatically approved and merged.
+
+
+on:
+  check_suite:
+    types: [completed]
+  pull_request:
+    types: [labeled]
+  workflow_dispatch:
+  status:
+
+permissions: write-all
+
+jobs:
+  setup:
+    name: setup
+    runs-on: ubuntu-latest
+    outputs:
+      BSC_DATABASESECRET: ${{ steps.setenvvars.outputs.BSC_DATABASESECRET }}
+    steps:
+      - name: Check out repository code
+        uses: actions/checkout@v2
+      - name: Check if main & set dev/prod vars
+        id: setenvvars
+        run: |
+          if [ ${{ github.ref }} != 'refs/heads/main' ]; then
+            echo "is NOT main branch"
+            echo "::set-output name=BSC_DATABASESECRET:: ${{ secrets.BSC_DATABASESECRET_DEV }}"
+          else
+            echo "is main branch"
+            echo "::set-output name=BSC_DATABASESECRET:: ${{ secrets.BSC_DATABASESECRET_PROD }}"
+          fi
+
+  isautomerge:
+    name: Check if PR is labeled with 'automerge' or 'automerge-web'
+    runs-on: ubuntu-latest
+    if: |
+      contains( github.event.pull_request.labels.*.name, 'automerge') ||
+      contains( github.event.pull_request.labels.*.name, 'automerge-web')
+    outputs:
+      AUTOMERGE: ${{ steps.setautomerge.outputs.AUTOMERGE }}
+    steps:
+      - name: Set 'automerge' to 'True' # job only runs if True
+        id: setautomerge
+        run: |
+          echo "::set-output name=AUTOMERGE::True"
+
+  travis_success:
+    name: Check if Travis build is successful
+    runs-on: ubuntu-latest
+    needs: [isautomerge]
+    if: ${{ needs.isautomerge.outputs.AUTOMERGE == 'True' }}
+    outputs:
+      TRAVIS_OK: ${{ steps.istravisok.outputs.TRAVIS_OK }}
+    steps:
+      - name: Get Travis build status
+        id: gettravisstatus
+        run: |
+          echo ${{ github.event.pull_request.head.sha }}
+          echo "TRAVIS_CONCLUSION=$(curl -X GET https://api.github.com/repos/brain-score/language/commits/${{ github.event.pull_request.head.sha }}/check-runs | python -c "import sys,json; print(json.load(sys.stdin)['check_runs'][0]['conclusion'])")" >> $GITHUB_ENV
+      - name: Check if Travis was successful
+        id: istravisok
+        run: |
+          if [ "$TRAVIS_CONCLUSION" == "success" ]
+          then
+              travisok='True'
+          # elif [ "$TRAVIS_CONCLUSION" == "None" ]
+          # then
+          #     travisok='Wait'
+          else
+              travisok='False'
+          fi
+          echo "::set-output name=TRAVIS_OK::$travisok"
+      - name: Sanity check
+        run: echo ${{ join(github.event.pull_request.labels.*.name, ',') }}
+
+  email_on_failure:
+    name: If tests fail on an automerge-web PR, send email to submitter
+    runs-on: ubuntu-latest
+    needs: [setup, travis_success]
+    env: 
+      BSC_DATABASESECRET: ${{ needs.setup.outputs.BSC_DATABASESECRET }}
+    if: |
+        needs.travissuccess.outputs.TRAVIS_OK == 'False' &&
+        join(github.event.pull_request.labels.*.name, ',') == 'automerge-web'
+    steps:
+    - name: Parse Brain-Score user ID from PR title
+      run: |
+        echo "BS_UID="$(<<<${{ github.event.pull_request.title }} | sed -E 's/.*\(user:([^)]+)\).*/\1/'"" >> $GITHUB_ENV
+    - name: Build project
+      run: |
+        python -m pip install "."
+    - name: Get email from Brain-Score UID
+      run: |
+        echo "TO_EMAIL=$(python .github/workflows/call_endpoints.py get_user_email '${{ env.BS_UID }}')" >> $GITHUB_ENV
+    - name: Send email to Brain-Score user
+      uses: dawidd6/action-send-mail@v3
+      with:
+        # Required mail server address:
+        server_address: smtp.gmail.com
+        # Server port, default 25:
+        server_port: 465
+        # Optional whether this connection use TLS (default is true if server_port is 465)
+        secure: true
+        # Optional (recommended) mail server username:
+        username: ${{ secrets.MAIL_USERNAME }}
+        # Optional (recommended) mail server password:
+        password: ${{ secrets.MAIL_PASSWORD }}
+        # Required mail subject:
+        subject: Brain-Score submission failed
+        # Required recipients' addresses:
+        to: ${{ env.BS_UID }}
+        # Required sender full name:
+        from: Brain-Score
+        # Optional plain body:
+        body: Your Brain-Score submission did not pass checks. Please review the test results and update the PR at https://github.com/brain-score/language/pull/${{ github.event.number }} or send in an updated submission via the website.
+
+  checkchanges:
+    name: Check if PR only changes plugin files
+    runs-on: ubuntu-latest
+    needs: travis_success
+    if: ${{ needs.travissuccess.outputs.TRAVIS_OK == 'True' }}
+    outputs:
+      PLUGIN_INFO: ${{ steps.getpluginfo.outputs.PLUGIN_INFO }}
+      IS_PLUGIN_ONLY: ${{ steps.ispluginonly.outputs.IS_PLUGIN_ONLY }}
+    steps:
+      - name: Check out repository code
+        uses: actions/checkout@v2
+
+      - name: Get changed files
+        uses: dorny/paths-filter@v2.11.1
+        id: filter
+        with:
+          list-files: shell
+          filters: |
+            changed:
+              - '**'
+      - name: Save changed files to env var
+        run: echo "CHANGED_FILES=${{ steps.filter.outputs.changed_files }}" >> $GITHUB_ENV
+      
+      - name: Parse changed files with python script
+        run: |
+          echo "PLUGIN_INFO=$(python .github/workflows/parse_changed_files.py '${{ env.CHANGED_FILES }}')" >> $GITHUB_ENV
+          
+      - name: Save plugin info to outputs
+        id: getpluginfo
+        run: |
+          echo "PLUGIN_INFO=$PLUGIN_INFO" >> $GITHUB_OUTPUT
+
+      - name: check if plugin-only
+        id: ispluginonly
+        run: |
+          echo "IS_PLUGIN_ONLY=$(jq -r '.is_plugin_only' <<< "$PLUGIN_INFO")" >> $GITHUB_OUTPUT
+ 
+  automerge:
+    name: If plugin-only, approve and merge
+    runs-on: ubuntu-latest
+    needs: checkchanges
+    if: ${{ needs.checkchanges.outputs.IS_PLUGIN_ONLY == 'True' }}
+    steps:
+      - name: Auto Approve
+        uses: hmarr/auto-approve-action@v3.1.0
+
+      - name: Auto Merge
+        uses: plm9606/automerge_actions@1.2.2
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}

brainscore_language/submission/endpoints.py
@@ -1,0 +1,125 @@
+import argparse
+from typing import List, Union, Dict
+
+from brainscore_core import Score, Benchmark
+from brainscore_core.submission import UserManager, RunScoringEndpoint, DomainPlugins
+from brainscore_language import load_model, load_benchmark, score
+from brainscore_language.submission import config
+
+
+def call_jenkins(plugin_info: Dict[str, Union[List[str], str]]):
+    """
+    Triggered when changes are merged to the GitHub repository, if those changes affect benchmarks or models.
+    Starts run to score models on benchmarks (`run_scoring`).
+    """
+    jenkins_base = "http://braintree.mit.edu:8080"
+    jenkins_user = os.environ['JENKINS_USER']
+    jenkins_token = os.environ['JENKINS_TOKEN']
+    jenkins_trigger = os.environ['JENKINS_TRIGGER']
+    jenkins_job = "dev_score_plugins"
+
+    url = f'{jenkins_base}/job/{jenkins_job}/buildWithParameters?token={jenkins_trigger}'
+    payload = {k: v for k, v in plugin_info.items() if plugin_info[k]}
+    auth_basic = HTTPBasicAuth(username=jenkins_user, password=jenkins_token)
+    r = requests.get(url, params=payload, auth=auth_basic)
+    logger.debug(r)
+
+
+class LanguagePlugins(DomainPlugins):
+    def load_model(self, model_identifier: str):
+        return load_model(model_identifier)
+
+    def load_benchmark(self, benchmark_identifier: str) -> Benchmark:
+        return load_benchmark(benchmark_identifier)
+
+    def score(self, model_identifier: str, benchmark_identifier: str) -> Score:
+        return score(model_identifier, benchmark_identifier)
+
+
+language_plugins = LanguagePlugins()
+run_scoring_endpoint = RunScoringEndpoint(language_plugins, db_secret=config.get_database_secret())
+
+
+def get_user_email(uid: int) -> str:
+    """ Convenience method for GitHub Actions to get a user's email if their web-submitted PR fails. """
+    return get_email_from_uid(uid)
+
+
+def create_user(domain: str, email: str) -> int:
+    user_manager = UserManager(domain, email, db_secret=config.get_database_secret())
+    new_user_id = user_manager()
+    return new_user_id
+
+
+def _get_ids(args_dict: Dict[str, Union[str, List]], key: str) -> Union[List, str, None]:
+    return args_dict[key] if key in args_dict else None
+
+
+def _clean_args(remove_keys: List[str], args_dict: Dict[str, Union[str, List]]) -> Dict[str, Union[str, List]]:
+    return {k: v for k, v in args_dict.items() if k not in remove_keys}  # preserve other keys, e.g. `run_score`
+
+
+def run_scoring(args_dict: Dict[str, Union[str, List]]):
+    """ prepares `args_dict` as parameters for the `run_scoring_endpoint`. """
+    new_models = _get_ids(args_dict, 'new_models')
+    new_benchmarks = _get_ids(args_dict, 'new_benchmarks')
+
+    if args_dict['specified_only']:
+        assert len(new_models) > 0, "No models specified"
+        assert len(new_benchmarks) > 0, "No benchmarks specified"
+        args_dict['models'] = new_models
+        args_dict['benchmarks'] = new_benchmarks
+    else:
+        if new_models and new_benchmarks:
+            args_dict['models'] = RunScoringEndpoint.ALL_PUBLIC
+            args_dict['benchmarks'] = RunScoringEndpoint.ALL_PUBLIC
+        elif new_benchmarks:
+            args_dict['models'] = RunScoringEndpoint.ALL_PUBLIC
+            args_dict['benchmarks'] = new_benchmarks
+        elif new_models:
+            args_dict['models'] = new_models
+            args_dict['benchmarks'] = RunScoringEndpoint.ALL_PUBLIC
+
+    new_args = _clean_args(['new_benchmarks', 'new_models', 
+                            'author_email', 'specified_only'], args_dict)
+    new_args["domain"] = "language"
+
+    run_scoring_endpoint(**new_args)
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('jenkins_id', type=int,
+                        help='The id of the current jenkins run')
+    parser.add_argument('domain', type=str,
+                        help='The submission domain (vision or language)')
+    parser.add_argument('user_id', type=int, nargs='?', default=None,
+                        help='ID of submitting user in the postgres DB')
+    parser.add_argument('author_email', type=str, nargs='?', default=None,
+                        help='email associated with PR author GitHub username')
+    parser.add_argument('model_type', type=str, nargs='?', default='artificialsubject',
+                        help='Type of model to score')
+    parser.add_argument('public', type=bool, nargs='?', default=True,
+                        help='Public (or private) submission?')
+    parser.add_argument('competition', type=str, nargs='?', default=None,
+                        help='Name of competition for which submission is being scored')
+    parser.add_argument('--new_models', type=str, nargs='*', default=None,
+                        help='The identifiers of newly submitted models to score on all benchmarks')
+    parser.add_argument('--new_benchmarks', type=str, nargs='*', default=None,
+                        help='The identifiers of newly submitted benchmarks on which to score all models')
+    parser.add_argument('--specified_only', type=bool, nargs='?', default=False,
+                        help='Only score the plugins specified by new_models and new_benchmarks')
+    args, remaining_args = parser.parse_known_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    args_dict = vars(args)
+
+    if 'user_id' not in args_dict:
+        new_user_id = create_user(args_dict['domain'], args_dict['author_email'])
+        args_dict['user_id'] = new_user_id
+    
+    run_scoring(new_args)

tests/test_submission/test_endpoints.py
@@ -1,0 +1,77 @@
+import logging
+import numpy as np
+import pytest
+from pytest import approx
+
+""" the mock import has to be before importing endpoints so that the database is properly mocked """
+from .mock_config import test_database
+
+from brainscore_core.submission import database_models
+from brainscore_core.submission.database import connect_db
+from brainscore_core.submission.database_models import clear_schema
+from brainscore_language.submission.endpoints import run_scoring
+
+logger = logging.getLogger(__name__)
+
+
+class TestRunScoring:
+    @classmethod
+    def setup_class(cls):
+        logger.info('Connect to database')
+        connect_db(test_database)
+        clear_schema()
+
+    def setup_method(self):
+        logger.info('Initialize database entries')
+        database_models.User.create(id=1, email='test@brainscore.com', password='abcde',
+                                    is_active=True, is_staff=False, is_superuser=False, last_login='2022-10-14 9:25:00')
+
+    def teardown_method(self):
+        logger.info('Clean database')
+        clear_schema()
+
+    def test_successful_run(self):
+        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'artificialsubject', 
+                    'public': True, 'competition': 'None', 'new_models': ['randomembedding-100'], 
+                    'new_benchmarks': ['Pereira2018.243sentences-linear'], 'specified_only': True}
+        run_scoring(args_dict)
+        score_entries = database_models.Score.select()
+        score_entries = list(score_entries)
+        assert len(score_entries) == 1
+        score_entry = score_entries[0]
+        assert score_entry.score_ceiled == approx(.0285022, abs=0.005)
+
+    @pytest.mark.travis_slow
+    def test_multiple_models(self):
+        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'artificialsubject', 
+                    'public': True, 'competition': 'None', 'new_models': ['randomembedding-100', 'randomembedding-1600'], 
+                    'new_benchmarks': ['Pereira2018.243sentences-linear'], 'specified_only': True}
+        run_scoring(args_dict)
+        score_entries = database_models.Score.select()
+        assert len(score_entries) == 2
+        score_values = [entry.score_ceiled for entry in score_entries]
+        assert all(np.array(score_values) > 0)
+
+    def test_benchmark_does_not_exist(self):
+        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'artificialsubject', 
+                    'public': True, 'competition': 'None', 'new_models': ['randomembedding-100'], 
+                    'new_benchmarks': ['idonotexist'], 'specified_only': True}
+        run_scoring(args_dict)
+        score_entries = database_models.Score.select()
+        assert len(score_entries) == 0
+
+    def test_model_cannot_run(self):
+        # embedding model cannot perform behavior
+        args_dict = {'jenkins_id': 62, 'user_id': 1, 'model_type': 'artificialsubject', 
+                    'public': True, 'competition': 'None', 'new_models': ['randomembedding-100'], 
+                    'new_benchmarks': ['Futrell2018-pearsonr'], 'specified_only': True}
+        run_scoring(args_dict)
+        score_entries = database_models.Score.select()
+        score_entries = list(score_entries)
+        assert len(score_entries) == 1
+        score_entry = score_entries[0]
+        assert score_entry.score_ceiled is None
+        assert score_entry.score_raw is None
+        assert score_entry.error is None
+        assert 'NotImplementedError' in score_entry.comment
+        assert 'embedding.py' in score_entry.comment  # test for some stacktrace

.github/workflows/call_endpoints.py
@@ -1,0 +1,10 @@
+import ast
+import sys
+
+from brainscore_language.submission.endpoints import call_jenkins, get_email_from_uid
+
+
+if __name__ == '__main__':
+	function = getattr(sys.modules[__name__], sys.argv[1])
+	args = ast.literal_eval(sys.argv[2])
+	function(args)

commit 021feda63b15edd6010b784939995eb86ad805d3

Fixes bugs introduced by #108 (#174)

.github/workflows/automerge_plugin-only_prs.yml
@@ -14,11 +14,8 @@ name: Automatically merge plugin-only PRs
 
 
 on:
-  check_suite:
-    types: [completed]
   pull_request:
     types: [labeled]
-  workflow_dispatch:
   status:
 
 permissions: write-all
@@ -174,7 +171,7 @@ jobs:
       - name: Auto Merge (GitHub submissions)
         uses: plm9606/automerge_actions@1.2.2
         with:
-          github-token: ${{ secrets.GITHUB_TOKEN }}
+          github-token: ${{ secrets.WORKFLOW_TOKEN }}
           label-name: "automerge"
           merge-method: "squash"
           auto-delete: "true"
@@ -182,7 +179,7 @@ jobs:
       - name: Auto Merge (brain-score.org submissions)
         uses: plm9606/automerge_actions@1.2.2
         with:
-          github-token: ${{ secrets.GITHUB_TOKEN }}
+          github-token: ${{ secrets.WORKFLOW_TOKEN }}
           label-name: "automerge-web"
           merge-method: "squash"
           auto-delete: "true"

.github/workflows/travis_trigger.sh
@@ -1,0 +1,10 @@
+#!/bin/bash
+
+GH_WORKFLOW_TRIGGER=$1
+TRAVIS_PULL_REQUEST_SHA=$2
+
+curl -L -X POST \
+-H "Authorization: token $GH_WORKFLOW_TRIGGER" \
+-d '{"state": "success", "description": "Run automerge workflow", 
+  "context": "continuous-integration/travis"}' \
+  "https://api.github.com/repos/brain-score/language/statuses/$TRAVIS_PULL_REQUEST_SHA"

.travis.yml
@@ -1,13 +1,17 @@
 language: python
-python:
-  - "3.7"
-  - "3.8"
-  - "3.9"
+matrix:
+  include:
+    - name: 3.8 public
+      python: '3.8'
+    - name: 3.8 private
+      python: '3.8'
+      env: PRIVATE_ACCESS=1
+    - name: 3.9 public
+      python: '3.9'
+    - name: 3.9 private
+      python: '3.9'
+      env: PRIVATE_ACCESS=1
 install:
-  # fix setuptools for python 3.7, error in other versions https://github.com/pypa/setuptools/issues/3293
-  - if [[ $TRAVIS_PYTHON_VERSION == 3.7* ]]; then
-    pip install "setuptools==60.5.0";
-    fi
   - python -m pip install -e ".[test]"
   # install conda for plugin runner
   - wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
@@ -21,5 +25,12 @@ install:
   # install singularity for container models
   - conda install -yc conda-forge singularity
 script:
-  - pytest -m "not requires_gpu and not memory_intense and not slow and not travis_slow"
+  - if [ "$PRIVATE_ACCESS" = 1 ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then pytest -m "private_access and not requires_gpu and not memory_intense and not slow and not travis_slow"; fi
+  - if [ "$PRIVATE_ACCESS" != 1 ]; then pytest -m "not private_access and not requires_gpu and not memory_intense and not slow and not travis_slow"; fi
   - python -c "from brainscore_core.plugin_management.test_plugins import run_args; run_args('brainscore_language')"
+
+jobs:
+  include:
+    # trigger workflow to check if plugin is being added
+    - stage: "Trigger automerge workflow"
+      script: if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then bash .github/workflows/travis_trigger $GH_WORKFLOW_TRIGGER $TRAVIS_PULL_REQUEST_SHA; fi

brainscore_language/submission/endpoints.py
@@ -55,60 +55,51 @@ def _get_ids(args_dict: Dict[str, Union[str, List]], key: str) -> Union[List, st
     return args_dict[key] if key in args_dict else None
 
 
-def _clean_args(remove_keys: List[str], args_dict: Dict[str, Union[str, List]]) -> Dict[str, Union[str, List]]:
-    return {k: v for k, v in args_dict.items() if k not in remove_keys}  # preserve other keys, e.g. `run_score`
-
-
 def run_scoring(args_dict: Dict[str, Union[str, List]]):
-    """ prepares `args_dict` as parameters for the `run_scoring_endpoint`. """
+    """ prepares parameters for the `run_scoring_endpoint`. """
     new_models = _get_ids(args_dict, 'new_models')
     new_benchmarks = _get_ids(args_dict, 'new_benchmarks')
 
     if args_dict['specified_only']:
         assert len(new_models) > 0, "No models specified"
         assert len(new_benchmarks) > 0, "No benchmarks specified"
-        args_dict['models'] = new_models
-        args_dict['benchmarks'] = new_benchmarks
+        models = new_models
+        benchmarks = new_benchmarks
     else:
         if new_models and new_benchmarks:
-            args_dict['models'] = RunScoringEndpoint.ALL_PUBLIC
-            args_dict['benchmarks'] = RunScoringEndpoint.ALL_PUBLIC
+            models = RunScoringEndpoint.ALL_PUBLIC
+            benchmarks = RunScoringEndpoint.ALL_PUBLIC
         elif new_benchmarks:
-            args_dict['models'] = RunScoringEndpoint.ALL_PUBLIC
-            args_dict['benchmarks'] = new_benchmarks
+            models = RunScoringEndpoint.ALL_PUBLIC
+            benchmarks = new_benchmarks
         elif new_models:
-            args_dict['models'] = new_models
-            args_dict['benchmarks'] = RunScoringEndpoint.ALL_PUBLIC
+            models = new_models
+            benchmarks = RunScoringEndpoint.ALL_PUBLIC
 
-    new_args = _clean_args(['new_benchmarks', 'new_models', 
-                            'author_email', 'specified_only'], args_dict)
-    new_args["domain"] = "language"
-
-    run_scoring_endpoint(**new_args)
+    run_scoring_endpoint(domain="language", jenkins_id=args_dict["jenkins_id"], 
+        models=models, benchmarks=benchmarks, user_id=args_dict["user_id"], 
+        model_type="artificialsubject", public=args_dict["public"], 
+        competition=args_dict["competition"])
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('jenkins_id', type=int,
                         help='The id of the current jenkins run')
-    parser.add_argument('domain', type=str,
-                        help='The submission domain (vision or language)')
-    parser.add_argument('user_id', type=int, nargs='?', default=None,
-                        help='ID of submitting user in the postgres DB')
-    parser.add_argument('author_email', type=str, nargs='?', default=None,
-                        help='email associated with PR author GitHub username')
-    parser.add_argument('model_type', type=str, nargs='?', default='artificialsubject',
-                        help='Type of model to score')
     parser.add_argument('public', type=bool, nargs='?', default=True,
                         help='Public (or private) submission?')
-    parser.add_argument('competition', type=str, nargs='?', default=None,
+    parser.add_argument('--competition', type=str, nargs='?', default=None,
                         help='Name of competition for which submission is being scored')
+    parser.add_argument('--user_id', type=int, nargs='?', default=None,
+                        help='ID of submitting user in the postgres DB')
+    parser.add_argument('--author_email', type=str, nargs='?', default=None,
+                        help='email associated with PR author GitHub username')
+    parser.add_argument('--specified_only', type=bool, nargs='?', default=False,
+                        help='Only score the plugins specified by new_models and new_benchmarks')
     parser.add_argument('--new_models', type=str, nargs='*', default=None,
                         help='The identifiers of newly submitted models to score on all benchmarks')
     parser.add_argument('--new_benchmarks', type=str, nargs='*', default=None,
                         help='The identifiers of newly submitted benchmarks on which to score all models')
-    parser.add_argument('--specified_only', type=bool, nargs='?', default=False,
-                        help='Only score the plugins specified by new_models and new_benchmarks')
     args, remaining_args = parser.parse_known_args()
 
     return args

tests/test_submission/test_endpoints.py
@@ -14,6 +14,7 @@ from brainscore_language.submission.endpoints import run_scoring
 logger = logging.getLogger(__name__)
 
 
+@pytest.mark.private
 class TestRunScoring:
     @classmethod
     def setup_class(cls):