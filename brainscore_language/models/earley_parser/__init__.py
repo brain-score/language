280343809f4a2f1646ceda764525f15666c342b1
Khaled Shehada <45083797+shehadak@users.noreply.github.com>
2023-06-01 01:45:43 -0400

    Changed the model identifier to 'earley-parser-minivocab' (#190)

--- brainscore_language/models/earley_parser/__init__.py
+++ brainscore_language/models/earley_parser/__init__.py
@@ -1,4 +1,4 @@
 from brainscore_language import model_registry
 from brainscore_language.models.earley_parser.parser import EarleyParserSubject
 
-model_registry["earley-parser"] = EarleyParserSubject
+model_registry["earley-parser-minivocab"] = EarleyParserSubject
