6d3b322d528cee06b28daf8dc774c7e31a2a586e
Khai Loong Aw <awkhailoong@gmail.com>
2023-07-13 14:24:57 +0200

    Fix model_id for T5 models

--- brainscore_language/models/t5/__init__.py
+++ brainscore_language/models/t5/__init__.py
@@ -1,76 +1,66 @@
 from brainscore_language import model_registry
 from brainscore_language import ArtificialSubject
 from brainscore_language.model_helpers.huggingface import HuggingfaceSubject
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import AutoModelForSeq2SeqLM
 
 # layer assignment based on choosing the maximally scoring layer on Pereira2018-encoding
 
 model_registry['t5-small'] = lambda: HuggingfaceSubject(
-    model_id='t5-small', 
+    model_id='google/t5-v1_1-small', 
     model=AutoModelForSeq2SeqLM.from_pretrained('google/t5-v1_1-small', device_map="auto"),
-    tokenizer=AutoTokenizer.from_pretrained('google/t5-v1_1-small'),
     region_layer_mapping={ArtificialSubject.RecordingTarget.language_system: 'decoder.block.6'}
 )
 
 model_registry['t5-base'] = lambda: HuggingfaceSubject(
-    model_id='t5-base', 
+    model_id='google/t5-v1_1-base', 
     model=AutoModelForSeq2SeqLM.from_pretrained('google/t5-v1_1-base', device_map="auto"),
-    tokenizer=AutoTokenizer.from_pretrained('google/t5-v1_1-base'),
     region_layer_mapping={ArtificialSubject.RecordingTarget.language_system: 'encoder.block.9'}
 )
 
 model_registry['t5-large'] = lambda: HuggingfaceSubject(
-    model_id='t5-large', 
+    model_id='google/t5-v1_1-large', 
     model=AutoModelForSeq2SeqLM.from_pretrained('google/t5-v1_1-large', device_map="auto"),
-    tokenizer=AutoTokenizer.from_pretrained('google/t5-v1_1-large'),
     region_layer_mapping={ArtificialSubject.RecordingTarget.language_system: 'encoder.block.17'}
 )
 
 model_registry['t5-xl'] = lambda: HuggingfaceSubject(
-    model_id='t5-xl', 
+    model_id='google/t5-v1_1-xl', 
     model=AutoModelForSeq2SeqLM.from_pretrained('google/t5-v1_1-xl', device_map="auto"),
-    tokenizer=AutoTokenizer.from_pretrained('google/t5-v1_1-xl'),
     region_layer_mapping={ArtificialSubject.RecordingTarget.language_system: 'decoder.block.2'}
 )
 
 model_registry['t5-xxl'] = lambda: HuggingfaceSubject(
-    model_id='t5-xxl', 
+    model_id='google/t5-v1_1-xxl', 
     model=AutoModelForSeq2SeqLM.from_pretrained('google/t5-v1_1-xxl', device_map="auto"),
-    tokenizer=AutoTokenizer.from_pretrained('google/t5-v1_1-xxl'),
     region_layer_mapping={ArtificialSubject.RecordingTarget.language_system: 'decoder.block.0'}
 )
 
 model_registry['flan-t5-small'] = lambda: HuggingfaceSubject(
-    model_id='flan-t5-small', 
+    model_id='google/flan-t5-small', 
     model=AutoModelForSeq2SeqLM.from_pretrained('google/flan-t5-small', device_map="auto"),
-    tokenizer=AutoTokenizer.from_pretrained('google/flan-t5-small'),
     region_layer_mapping={ArtificialSubject.RecordingTarget.language_system: 'encoder.block.7'}
 )
 
 model_registry['flan-t5-base'] = lambda: HuggingfaceSubject(
-    model_id='flan-t5-base', 
+    model_id='google/flan-t5-base', 
     model=AutoModelForSeq2SeqLM.from_pretrained('google/flan-t5-base', device_map="auto"),
-    tokenizer=AutoTokenizer.from_pretrained('google/flan-t5-base'),
     region_layer_mapping={ArtificialSubject.RecordingTarget.language_system: 'decoder.block.7'}
 )
 
 model_registry['flan-t5-large'] = lambda: HuggingfaceSubject(
-    model_id='flan-t5-large', 
+    model_id='google/flan-t5-large', 
     model=AutoModelForSeq2SeqLM.from_pretrained('google/flan-t5-large', device_map="auto"),
-    tokenizer=AutoTokenizer.from_pretrained('google/flan-t5-large'),
     region_layer_mapping={ArtificialSubject.RecordingTarget.language_system: 'encoder.block.18'}
 )
 
 model_registry['flan-t5-xl'] = lambda: HuggingfaceSubject(
-    model_id='flan-t5-xl', 
+    model_id='google/flan-t5-xl', 
     model=AutoModelForSeq2SeqLM.from_pretrained('google/flan-t5-xl', device_map="auto"),
-    tokenizer=AutoTokenizer.from_pretrained('google/flan-t5-xl'),
     region_layer_mapping={ArtificialSubject.RecordingTarget.language_system: 'decoder.block.2'}
 )
 
 model_registry['flan-t5-xxl'] = lambda: HuggingfaceSubject(
-    model_id='flan-t5-xxl', 
+    model_id='google/flan-t5-xxl', 
     model=AutoModelForSeq2SeqLM.from_pretrained('google/flan-t5-xxl', device_map="auto"),
-    tokenizer=AutoTokenizer.from_pretrained('google/flan-t5-xxl'),
     region_layer_mapping={ArtificialSubject.RecordingTarget.language_system: 'decoder.block.0'}
 )
