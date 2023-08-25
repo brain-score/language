d1f0fa6801832ea9e5d5126ad47a2641e564e1f2
Katherine Fairchild <kvfairchild@gmail.com>
2023-08-25 16:04:46 -0400

    add after_failure automerge check

--- .github/workflows/travis_trigger.sh
+++ .github/workflows/travis_trigger.sh
@@ -2,9 +2,10 @@
 
 GH_WORKFLOW_TRIGGER=$1
 TRAVIS_PULL_REQUEST_SHA=$2
+STATUS=$3
 
 curl -L -X POST \
 -H "Authorization: token $GH_WORKFLOW_TRIGGER" \
--d '{"state": "success", "description": "Run automerge workflow", 
+-d '{"state": "'$STATUS'", "description": "Run automerge workflow", 
   "context": "continuous-integration/travis"}' \
   "https://api.github.com/repos/brain-score/language/statuses/$TRAVIS_PULL_REQUEST_SHA"
