a6421332d5fc2d83ce5969da500521c65ab1dfd8
Khaled Shehada <45083797+shehadak@users.noreply.github.com>
2023-11-16 12:57:51 -0500

    Added GPT2-Large model (#213)
    * Added GPT-Large model
    
    * Added unit tests for GPT2-Large model

--- brainscore_language/models/gpt/__init__.py
+++ brainscore_language/models/gpt/__init__.py
@@ -8,6 +8,9 @@ from brainscore_language.model_helpers.huggingface import HuggingfaceSubject
 model_registry['distilgpt2'] = lambda: HuggingfaceSubject(model_id='distilgpt2', region_layer_mapping={
     ArtificialSubject.RecordingTarget.language_system: 'transformer.h.5'})
 
+model_registry['gpt2-large'] = lambda: HuggingfaceSubject(model_id='gpt2-large', region_layer_mapping={
+    ArtificialSubject.RecordingTarget.language_system: 'transformer.h.33'})
+
 model_registry['gpt2-xl'] = lambda: HuggingfaceSubject(model_id='gpt2-xl', region_layer_mapping={
     ArtificialSubject.RecordingTarget.language_system: 'transformer.h.43'})
 
