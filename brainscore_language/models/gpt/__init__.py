a7529066be5a6504f77945808a56f8c2302b2f0a
Khaled K Shehada <shehadak@mit.edu>
2023-11-17 14:31:21 -0500

    Added comment

--- brainscore_language/models/gpt/__init__.py
+++ brainscore_language/models/gpt/__init__.py
@@ -5,6 +5,7 @@ from brainscore_language.model_helpers.huggingface import HuggingfaceSubject
 # layer assignment based on choosing the maximally scoring layer on Pereira2018-encoding from
 # https://github.com/mschrimpf/neural-nlp/blob/master/precomputed-scores.csv
 
+# GPT-1
 model_registry['openai-gpt'] = lambda: HuggingfaceSubject(model_id='openai-gpt', region_layer_mapping={
     ArtificialSubject.RecordingTarget.language_system: 'transformer.h.11'})
 
