1289731cf773acd278634b9418a2b95feffb1f8b
Deirdre Kelliher <dmkelliher@gmail.com>
2024-02-05 16:36:07 -0500

    Removing unused CSS overrides for Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,12 +65,6 @@ bibtex_bibfiles = ['bibtex/refs.bib']
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_style = "overrides.css"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
 
 
 def skip(app, what, name, obj, would_skip, options):
