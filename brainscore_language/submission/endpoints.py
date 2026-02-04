737a8e332659c380584a56686b6662f79a9c7d7a
Michael Ferguson <mferg@mit.edu>
2026-02-04 13:15:21 -0500

    add temproary jenkins trigger test (#353)

--- brainscore_language/submission/endpoints.py
+++ brainscore_language/submission/endpoints.py
@@ -1,4 +1,8 @@
 from typing import List, Union, Dict
+import os
+import json
+import requests
+from requests.auth import HTTPBasicAuth
 
 from brainscore_core import Score, Benchmark
 from brainscore_core.submission import RunScoringEndpoint, DomainPlugins
@@ -39,6 +43,31 @@ def send_email_to_submitter(uid: int, domain: str, pr_number: str,
     send_email_to_submitter_core(uid=uid, domain=domain, pr_number=pr_number,
                                  db_secret=config.get_database_secret(),
                                  mail_username=mail_username, mail_password=mail_password)
+
+
+def call_jenkins_language(plugin_info: Union[str, Dict[str, Union[List[str], str]]]):
+    """
+    Language-specific Jenkins trigger that uses 'score_plugins' job instead of 'dev_score_plugins'.
+    Same as call_jenkins from core but with different job name and path.
+    """
+    jenkins_base = "http://www.brain-score-jenkins.com:8080"
+    jenkins_user = os.environ['JENKINS_USER']
+    jenkins_token = os.environ['JENKINS_TOKEN']
+    jenkins_trigger = os.environ['JENKINS_TRIGGER']
+    jenkins_job = "core/job/score_plugins"  # Language domain uses core/job/score_plugins instead of dev_score_plugins
+
+    url = f'{jenkins_base}/job/{jenkins_job}/buildWithParameters?token={jenkins_trigger}'
+
+    if isinstance(plugin_info, str):
+        # Check if plugin_info is a String object, in which case JSON-deserialize it into Dict
+        plugin_info = json.loads(plugin_info)
+
+    payload = {k: v for k, v in plugin_info.items() if plugin_info[k]}
+    try:
+        auth_basic = HTTPBasicAuth(username=jenkins_user, password=jenkins_token)
+        requests.get(url, params=payload, auth=auth_basic)
+    except Exception as e:
+        print(f'Could not initiate Jenkins job because of {e}')
 
 
 if __name__ == '__main__':
