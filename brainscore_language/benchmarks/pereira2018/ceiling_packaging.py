019823c6656ee2e9d63bff5acfe2f402fd8187e8
Kartik Pradeepan <kartikspradeepan@gmail.com>
2026-02-17 20:52:01 -0500

    update ridge ceilings

--- brainscore_language/benchmarks/pereira2018/ceiling_packaging.py
+++ brainscore_language/benchmarks/pereira2018/ceiling_packaging.py
@@ -219,7 +219,7 @@ class ExtrapolationCeiling:
 
             try:
                 params = self.fit(subject_subsamples, bootstrapped_scores)
-            except RuntimeError:  # optimal parameters not found
+            except (RuntimeError, ValueError):  # optimal parameters not found or NaN inputs
                 params = [np.nan, np.nan]
             params = DataAssembly([params], coords={'bootstrap': [bootstrap], 'param': ['v0', 'tau0']},
                                   dims=['bootstrap', 'param'])
@@ -245,10 +245,12 @@ class ExtrapolationCeiling:
         return score
 
     def fit(self, subject_subsamples, bootstrapped_scores):
-        valid = ~np.isnan(bootstrapped_scores)
+        subject_subsamples = np.array(subject_subsamples)
+        bootstrapped_scores = np.array(bootstrapped_scores)
+        valid = ~np.isnan(bootstrapped_scores) & np.isfinite(bootstrapped_scores)
         if sum(valid) < 1:
             raise RuntimeError("No valid scores in sample")
-        params, pcov = curve_fit(v, subject_subsamples, bootstrapped_scores,
+        params, pcov = curve_fit(v, subject_subsamples[valid], bootstrapped_scores[valid],
                                  # v (i.e. max ceiling) is between 0 and 1, tau0 unconstrained
                                  bounds=([0, -np.inf], [1, np.inf]))
         return params
